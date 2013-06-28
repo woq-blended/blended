72013802df03a95a518223b9e59579750bc5260b
Andreas Gies <andreas@wayofquality.de>
2013-06-28 14:38:21 +0200

    Added assembly basic definitions & empty assembly.

---
+++ de.woq.osgi.java.assembly/de.woq.osgi.java.assembly.defaults/src/main/resources/scripts/woqContainer.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Copyright 2013, WoQ - Way of Quality UG(mbH)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+OLD_PWD=`pwd`
+WOQ_HOME=`dirname $0`/..
+cd ${WOQ_HOME}
+WOQ_HOME=`pwd`
+cd ${OLD_PWD}
+
+JAVA=${JAVA_HOME}/bin/java
+
+if [[ ! -e ${JAVA} ]]; then
+  JAVA=`which java`
+fi
+
+if [[ ! -e ${JAVA} ]]; then
+  JAVA=${WOQ_HOME}/jre/bin/java
+fi
+
+if [[ ! -e ${JAVA} ]]; then
+  if [[ ! -z `echo ${OSTYPE} | grep "darwin"` ]]; then
+    JAVA=/System/Library/Frameworks/JavaVM.framework/Home/bin/java
+  fi  
+fi
+
+if [[ ! -e ${JAVA} ]]; then
+  echo ""
+  echo "Unable to locate a Java Virtual Machine. The container framework looks"
+  echo "in the following locations and will take the first available JVM it finds:"
+  echo ""
+  echo " 1 - A JVM referenced by the JAVA_HOME variable [${JAVA_HOME}]"
+  echo ""
+  echo " 2 - A JVM that is available from the current PATH environment variable [${PATH}]"
+  echo ""
+  echo " 3 - A JVM that is loacated in the JRE directory of the Whiteboard installation [${WHITEBOARD_HOME}/jre]"
+  echo ""
+  echo " 4 - On OS X in [/System/Library/Frameworks/JavaVM.framework/Home/bin]"
+  echo ""
+  echo "Please make a valid Java Virtual machine available in one of these locations."
+  echo ""
+  exit 1
+fi
+
+APPCP=${WOQ_HOME}/config
+
+for jar in $(ls $WOQ_HOME/lib/*.jar)
+do
+  APPCP=$APPCP:${jar}
+done
+
+echo "SIB Home directory is [${WOQ_HOME}]."
+${JAVA} -version
+
+${JAVA} -classpath $APPCP de.woq.osgi.java.container.Main -jvm.property.sib.home ${WOQ_HOME} $1
+
+exit $?
---
+++ de.woq.osgi.java.launcher/src/main/java/de/woq/osgi/java/container/Main.java
@@ -0,0 +1,214 @@
+package de.woq.osgi.java.container;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.ops4j.pax.runner.Run;
+
+public class Main implements ContainerConstants {
+
+  private Properties properties = null;
+  private String containerName = "empty";
+
+  private void launch(final String[] args) {
+
+    parseArguments(args);
+    System.out.println("Starting container configuration [" + getContainerName() + "]");
+
+    List<String> params = new ArrayList<String>();
+
+    params.add("--nologo=true");
+    params.add("--clean");
+
+    params.add("--platform=" + getPlatformVendor());
+    params.add("--version=" + getPlatformVersion());
+    params.add("--startLevel=" + getPlatformStartLevel());
+
+    params.add("--log=" + getPlatformLogLevel());
+
+    String jvmOptions = getJvmOptions();
+    if (jvmOptions != null)
+    {
+      params.add("--vmOptions=" + jvmOptions);
+    }
+
+    String sibHome = getPlatformProperties().getProperty(PROP_SIB_HOME);
+    if (sibHome != null)
+    {
+      params.add("--classpath=" + sibHome + "/config");
+    }
+
+    params.add("--dir=" + getContainerName());
+
+    params.add("classpath:" + getContainerName() + ".composite");
+
+    Run.main(params.toArray(new String[] {}));
+  }
+
+  private String getContainerName() {
+    return containerName;
+  }
+
+  private void setContainerName(String containerName) {
+    this.containerName = containerName;
+  }
+
+  private String getPlatformVendor()
+  {
+    return getPlatformProperties().getProperty(PROP_PLATFORM_VENDOR, DEFAULT_PLATFORM_VENDOR);
+  }
+
+  private String getPlatformVersion()
+  {
+    return getPlatformProperties().getProperty(PROP_PLATFORM_VERSION, DEFAULT_PLATFORM_VERSION);
+  }
+
+  private String getPlatformLogLevel()
+  {
+    return getPlatformProperties().getProperty(PROP_LOG_LEVEL, DEFAULT_LOG_LEVEL);
+  }
+
+  private String getPlatformStartLevel()
+  {
+    return getPlatformProperties().getProperty(PROP_PLATFORM_STARTLEVEL, DEFAULT_PLATFORM_STARTLEVEL);
+  }
+
+  private String getStackSize()
+  {
+    return getPlatformProperties().getProperty(JVM_STACK_SIZE, DEFAULT_JVM_STACK_SIZE);
+  }
+
+  private String getMinHeap()
+  {
+    return getPlatformProperties().getProperty(JVM_MIN_HEAP, DEFAULT_JVM_MIN_HEAP);
+  }
+
+  private String getMaxHeap()
+  {
+    return getPlatformProperties().getProperty(JVM_MIN_HEAP, DEFAULT_JVM_MAX_HEAP);
+  }
+
+  private String getJvmOptions()
+  {
+    StringBuilder optionString = new StringBuilder();
+
+    optionString.append("-Xss");
+    optionString.append(getStackSize());
+    optionString.append(" ");
+
+    optionString.append("-Xms");
+    optionString.append(getMinHeap());
+    optionString.append(" ");
+
+    optionString.append("-Xmx");
+    optionString.append(getMaxHeap());
+    optionString.append(" ");
+
+    for(String key: getPlatformProperties().stringPropertyNames())
+    {
+      if (key.startsWith(PARAM_SYSPROP))
+      {
+        String realKey = key.substring(PARAM_SYSPROP.length());
+        String value = getPlatformProperties().getProperty(key);
+        optionString.append("-D");
+        optionString.append(realKey);
+        optionString.append("=");
+        optionString.append(value);
+        optionString.append(" ");
+      }
+    }
+
+    String sibHome = getPlatformProperties().getProperty(PROP_SIB_HOME);
+    if (sibHome != null)
+    {
+      optionString.append("-D");
+      optionString.append(PROP_SIB_HOME);
+      optionString.append("=");
+      optionString.append(sibHome);
+      optionString.append(" ");
+    }
+
+    String debugPort = getPlatformProperties().getProperty(JVM_DEBUG_PORT);
+
+    if (debugPort != null)
+    {
+      String suspend = getPlatformProperties().getProperty(JVM_DEBUG_SUSPEND, "n");
+      optionString.append("-Xnoagent -Djava.compiler=NONE -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=");
+      optionString.append(suspend);
+      optionString.append(",address=");
+      optionString.append(debugPort);
+      optionString.append(" ");
+    }
+
+    return optionString.length() > 0 ? optionString.toString() : null;
+  }
+
+  private Properties getPlatformProperties() {
+
+    if (properties == null) {
+      properties = new Properties();
+      try {
+        System.out.println("Loading back to [" + CONTAINER_CFG_FILE + "]");
+        properties.load(Main.class.getResourceAsStream("/" + getContainerName() + "." + CONTAINER_CFG_FILE));
+      } catch(Exception e) {
+        try {
+          System.out.println("Falling back to [" + CONTAINER_CFG_FILE + "]");
+          properties.load(Main.class.getResourceAsStream("/" + CONTAINER_CFG_FILE));
+        } catch (Exception inner) {
+          System.out.println("Unable to load container properties file.");
+        }
+      }
+    }
+    return properties;
+  }
+
+  private void parseContainer(final String[] args) {
+
+    List<String> ctArgs  = new ArrayList<String>();
+
+    for (int i = 0; i<args.length; i++) {
+      if (args[i].startsWith("-" + PARAM_SYSPROP)) {
+        i++;
+      } else {
+        ctArgs.add(args[i]);
+      }
+    }
+
+    if (ctArgs.size() > 0) {
+      setContainerName(ctArgs.get(0));
+    }
+  }
+
+  private List<String> parseArguments(final String[] args) {
+
+    parseContainer(args);
+
+    List<String> result = new ArrayList<String>();
+
+    Properties props = getPlatformProperties();
+
+    for (int i = 0; i<args.length; i++)
+    {
+      if (args[i].startsWith("-" + PARAM_SYSPROP))
+      {
+        String propName = args[i];
+        i++;
+        String propValue = i < args.length ? args[i] : "";
+
+        props.setProperty(propName.substring(1), propValue);
+      }
+      else
+      {
+        result.add(args[i]);
+      }
+    }
+
+    return result;
+  }
+
+  public static void main(final String[] args)
+  {
+    new Main().launch(args);
+  }
+}
