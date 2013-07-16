b3abfb4715782c95df73305e69a9958d737e4123
Andreas Gies <andreas@wayofquality.de>
2013-07-16 08:55:19 +0200

    Added configuration for Pax Exam based integration tests.

---
+++ de.woq.osgi.java.itest/src/test/java/de/woq/osgi/java/itest/SimpleTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013, WoQ - Way of Quality UG(mbH)
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package de.woq.osgi.java.itest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy
+public class SimpleTest {
+
+  private final static Logger LOGGER = LoggerFactory.getLogger(SimpleTest.class);
+
+  @Configuration
+  public Option[] config() {
+    return options(
+      junitBundles()
+    );
+  }
+
+  @Test
+  public void simpleTest() {
+    LOGGER.info("Hello from my Test!");
+    Assert.assertTrue(true);
+  }
+}
