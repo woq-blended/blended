a836072f5a615e30c2ccaceb16691c47b2dc90a9
Andreas Gies <andreas@wayofquality.de>
2013-07-25 16:31:33 +0200

    Initial Angular Bundle Viewer structure.

---
+++ de.woq.osgi.java.webui.osgiconsole/src/main/resources/console/app/app.js
@@ -0,0 +1,25 @@
+﻿var bundleApp = angular.module('bundleApp', []);
+
+bundleApp.controller('BundleController', function($scope, bundleFactory) {
+
+  init();
+
+  function init() {
+    $scope.bundles = bundleFactory.getBundles();
+  }
+});
+
+bundleApp.factory('bundleFactory', function() {
+
+  var factory = {};
+
+  var bundles = [
+    { bundleId: 0, symbolicName: 'SystemBundle' }
+  ];
+
+  factory.getBundles = function() {
+    return bundles;
+  };
+
+  return factory;
+});
