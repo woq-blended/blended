3686fb9aa504c08076a473c7b23695634e2e2811
Andreas Gies <andreas@wayofquality.de>
2013-08-26 17:27:12 +0200

    Adding search capabilities for MBeans in Containers under test.

---
+++ de.woq.osgi.java.itestsupport/src/main/java/de/woq/osgi/java/itestsupport/condition/MBeanMatcher.java
@@ -0,0 +1,11 @@
+package de.woq.osgi.java.itestsupport.condition;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andreas
+ * Date: 8/26/13
+ * Time: 4:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MBeanMatcher {
+}
