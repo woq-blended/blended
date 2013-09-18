aacdb8547d04fbf9881aaf04d3b48c84a18c9ae8
Andreas Gies <andreas@wayofquality.de>
2013-09-18 10:06:52 +0200

    Initial projects for container registry & persistence.

---
+++ de.woq.osgi.java.container.registry/src/main/java/de/woq/java/container/registry/DefaultContainerInfo.java
@@ -0,0 +1,11 @@
+package de.woq.java.container.registry;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andreas
+ * Date: 9/18/13
+ * Time: 9:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultContainerInfo {
+}
