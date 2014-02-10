93df0e8c7583df3690c2d6ddec8f31371a37d323
Andreas Gies <andreas@wayofquality.de>
2014-02-10 11:00:49 +0100

    Fixing java path issue in installer.

--- de.woq.osgi.java.installer/src/main/resources/install-service.sh
+++ de.woq.osgi.java.installer/src/main/resources/install-service.sh
@@ -7,13 +7,13 @@ SERVICE_NAME=$1
 
 cd ${SCRIPT_HOME}/..
 KARAF_HOME=`pwd`
-JAVA_HOME=${KARAF_HOME}/jre
+KARAF_JAVA_HOME=${KARAF_HOME}/jre
 
 for jar in $(ls ${SCRIPT_HOME}/*.jar)
 do
   APPCP="${APPCP}:${jar}"
 done
 
-${JAVA_HOME}/bin/java -cp ${APPCP} -DKARAF_JAVA_HOME=${JAVA_HOME} de.woq.osgi.java.installer.ServiceInstaller -b ${KARAF_HOME} -n ${SERVICE_NAME}
+${KARAF_JAVA_HOME}/bin/java -cp ${APPCP} -DKARAF_JAVA_HOME=${JAVA_HOME} de.woq.osgi.java.installer.ServiceInstaller -b ${KARAF_HOME} -n ${SERVICE_NAME}
 
 cd ${OLD_DIR}
