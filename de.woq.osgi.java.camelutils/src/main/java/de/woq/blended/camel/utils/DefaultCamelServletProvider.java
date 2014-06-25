7cfc6cc5fee7e5279f532717011c4d313a85caad
Andreas Gies <andreas@wayofquality.de>
2014-06-25 18:05:24 +0200

    Renaming project to 'Blended'.

---
+++ de.woq.osgi.java.camelutils/src/main/java/de/woq/blended/camel/utils/DefaultCamelServletProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014ff, WoQ - Way of Quality UG(mbH)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.woq.blended.camel.utils;
+
+import org.apache.camel.component.http.CamelServlet;
+
+public class DefaultCamelServletProvider implements CamelServletProvider {
+
+  private CamelServlet camelServlet = null;
+
+  public void setCamelServlet(CamelServlet camelServlet) {
+    this.camelServlet = camelServlet;
+  }
+
+  @Override
+  public CamelServlet getCamelServlet() {
+    return camelServlet;
+  }
+}
---
+++ de.woq.osgi.java.camelutils/src/main/java/de/woq/blended/camel/utils/EndpointURIDispatcher.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014ff, WoQ - Way of Quality UG(mbH)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.woq.blended.camel.utils;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+
+public class EndpointURIDispatcher {
+
+  private final EndpointURIFactory uriFactory;
+
+  public EndpointURIDispatcher(EndpointURIFactory uriFactory) {
+    this.uriFactory = uriFactory;
+  }
+
+  public void dispatch(final Exchange exchange) throws Exception {
+
+    String[] endpointUris = uriFactory.createEndpointUris(exchange);
+
+    if (endpointUris == null || endpointUris.length == 0) {
+      throw new Exception("List of dispatch endpoints cannot be empty");
+    }
+
+    ProducerTemplate template = exchange.getContext().createProducerTemplate();
+    for(String uri: endpointUris) {
+      template.send(uri, exchange);
+    }
+  }
+}
