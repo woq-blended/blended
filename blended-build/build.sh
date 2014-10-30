531747e8482510fb73f087e6938adab0616f6177
Andreas Gies <andreas@wayofquality.de>
2014-10-30 17:12:58 +0000

    Update build.sh

--- blended-build/build.sh
+++ blended-build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean install -P parent | grep -v "^Download"
-mvn clean install -P build,assembly | grep -v "^Download"
+mvn clean install -P parent | grep -v "Download"
+mvn clean install -P build,assembly | grep -v "Download"
