6fd7e0a3fcf22601bc9921cae3a690c1e3b2a897
Andreas Gies <andreas@wayofquality.de>
2015-04-06 10:45:23 +0200

    Fixing Travis build script

--- blended-build/build.sh
+++ blended-build/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+cd blended-parent
+mvn clean install
+cd..
 mvn clean install -P parent | grep -v "Download" | grep -v "longer than 100"
 mvn clean install -P build,assembly | grep -v "Download" | grep -v "longer than 100"
