021a8bd18c250c2a35c87a3ec7128278ac6ebe3a
Tobias Roeser <le.petit.fou@web.de>
2015-05-20 15:05:27 +0200

    Use retval of first pipe command instead of the grep command

--- blended-build/build.sh
+++ blended-build/build.sh
@@ -3,5 +3,5 @@ set -e
 cd blended-parent
 mvn clean install
 cd ..
-mvn clean install -P parent | grep -v "Download" | grep -v "longer than 100"
-mvn clean install -P build,assembly | grep -v "Download" | grep -v "longer than 100"
+mvn clean install -P parent | grep -v "Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
+mvn clean install -P build,assembly | grep -v "Download" | grep -v "longer than 100"; test ${PIPESTATUS[0]} -eq 0
