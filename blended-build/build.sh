a5bcbcae4e56ec1d76068ac9099f49a1cb378a39
Tobias Roeser <le.petit.fou@web.de>
2015-05-20 16:04:17 +0200

    change shebang to /bin/bash as PIPESTATUS is bash specific

--- blended-build/build.sh
+++ blended-build/build.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 cd blended-parent
 mvn clean install
 cd ..
 mvn clean install -P parent | grep -v "Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
-mvn clean install -P build,assembly | grep -v "Download" | grep -v "longer than 100"; test ${PIPESTATUS[0]} -eq 0
+mvn clean install -P build,assembly | grep -v "Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
