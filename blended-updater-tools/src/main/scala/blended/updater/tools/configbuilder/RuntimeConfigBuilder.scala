0c71d6805bb08ba23ea828f47479b809d776ebc2
Tobias Roeser <le.petit.fou@web.de>
2015-06-15 15:11:22 +0200

    Removed unused cruft

--- blended-updater-tools/src/main/scala/blended/updater/tools/configbuilder/RuntimeConfigBuilder.scala
+++ blended-updater-tools/src/main/scala/blended/updater/tools/configbuilder/RuntimeConfigBuilder.scala
@@ -1,18 +1,13 @@
 package blended.updater.tools.configbuilder
 
-import de.tototec.cmdoption.CmdOption
-import java.net.URL
-import blended.updater.config.RuntimeConfig
-import java.net.URI
 import java.io.File
-import de.tototec.cmdoption.CmdCommand
-import scala.collection.immutable._
-import de.tototec.cmdoption.CmdlineParser
-import scala.util.Try
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigValueFactory
+
 import com.typesafe.config.ConfigFactory
+
 import blended.updater.config.ConfigWriter
+import blended.updater.config.RuntimeConfig
+import de.tototec.cmdoption.CmdOption
+import de.tototec.cmdoption.CmdlineParser
 
 object RuntimeConfigBuilder {
 
@@ -31,170 +26,6 @@ object RuntimeConfigBuilder {
 
     @CmdOption(names = Array("-f"), args = Array("configfile"))
     var configFile: String = ""
-  }
-
-  case class Options(
-    start: Boolean = false,
-    startLevel: Option[Int] = None,
-    sha1Sum: Option[String] = None,
-    fragment: Option[(String, String)] = None,
-    url: Option[String] = None)
-
-  class CmdlineApi {
-
-    private[this] var bundleOptions: Options = Options()
-
-    private[this] var bundles: Seq[Options] = Seq()
-
-    private[this] var profile: Option[(String, String)] = None
-
-    private[this] var output: Option[String] = None
-
-    private[this] var startLevel: Option[Int] = None
-
-    @CmdOption(names = Array("-L", "--start-level"), args = Array("level"), description = "Set the start level of the framework")
-    def frageworkStartLevel(startLevel: Int): Unit = this.startLevel = Option(startLevel)
-
-    private[this] var baseUrl: Option[String] = None
-    @CmdOption(names = Array("-B", "--base-url"), args = Array("URL"),
-      description = "Set a base URL used to locate bundles")
-    def setBaseUrl(url: String): Unit = {
-
-    }
-
-    @CmdOption(args = Array("URL"),
-      description = "Add a bundle from the given {0} (partial URLs are supported if a base URL is defined")
-    def addBundle(url: String): Unit = {
-      val bundleUrl = s"${baseUrl.map(_ + "/").getOrElse("")}${url}"
-      bundles :+= bundleOptions.copy(url = Option(bundleUrl))
-      bundleOptions = Options()
-    }
-
-    @CmdOption(names = Array("-s", "--start"), description = "Enable the start flag for the next bundle")
-    def nextBundleStart(): Unit = {
-      bundleOptions = bundleOptions.copy(start = true)
-    }
-
-    @CmdOption(names = Array("-l", "--bundle-start-level"), args = Array("level"), description = "Set the start level for the next bundle")
-    def nextBundleStartLevel(startLevel: Int): Unit = {
-      bundleOptions = bundleOptions.copy(startLevel = Option(startLevel))
-    }
-
-    @CmdOption(names = Array("-p", "--profile"), args = Array("name", "version"), description = "Set the name of this profile")
-    def setProfile(name: String, version: String): Unit = {
-      profile = Option(name -> version)
-    }
-
-    @CmdOption(names = Array("-o", "--output"), args = Array("file"), description = "To config file to be buit")
-    def setOutput(name: String): Unit = {
-      output = Option(name)
-    }
-
-    def build(): Try[RuntimeConfig] = Try {
-      val (name, version) = profile match {
-        case Some(x) => x
-        case None => throw new RuntimeException("Missing profile name and version configuration")
-      }
-      startLevel match {
-        case Some(x) => x
-        case None => throw new RuntimeException("Missing start level configuration")
-      }
-
-      val bundleConfigs = bundles.map { b =>
-
-        val bundleUrl = new URL(b.url.get)
-        val sha1Sum = b.sha1Sum.getOrElse {
-          // TODO: eval sha1Sum
-          ???
-        }
-
-        val bc = RuntimeConfig.BundleConfig(
-
-          url = bundleUrl.toExternalForm(),
-          jarName = new File(bundleUrl.getPath()).getName(),
-          sha1Sum = sha1Sum,
-          start = b.start,
-          startLevel = b.startLevel
-        )
-
-        b.fragment -> bc
-      }
-
-      val groupedBundles = bundleConfigs.toSeq.groupBy(b => b._1).mapValues(b => b.unzip._2)
-
-      val nonFragmentBundles = groupedBundles.getOrElse(None, Seq())
-
-      val fragments = groupedBundles.flatMap {
-        case (Some((n, v)), bs) =>
-          RuntimeConfig.FragmentConfig(
-            name = n,
-            version = v,
-            bundles = bs
-          ) :: Nil
-        case _ => Nil
-      }.toList
-
-      RuntimeConfig(
-        name = name,
-        version = version,
-        bundles = nonFragmentBundles,
-        fragments = fragments,
-        frameworkProperties = Map(),
-        systemProperties = Map(),
-        startLevel = ???,
-        defaultStartLevel = ???
-      )
-    }
-
-    @CmdCommand(names = Array("build"), description = "Build a new config file")
-    class Build(initialConfig: Config) {
-
-      private[this] var config: Config = initialConfig
-
-      @CmdOption(names = Array("-L", "--start-level"), args = Array("level"), description = "Set the start level of the framework")
-      def frageworkStartLevel(startLevel: Int): Unit =
-        config = config.withValue("startLevel", ConfigValueFactory.fromAnyRef(startLevel))
-
-      private[this] var baseUrl: Option[String] = None
-
-      @CmdOption(names = Array("-B", "--base-url"), args = Array("URL"),
-        description = "Set a base URL used to locate bundles")
-      def setBaseUrl(url: String): Unit = baseUrl = url.trim() match {
-        case "" => None
-        case x => Option(x)
-      }
-
-      private[this] var bundleOptions: Options = Options()
-
-      @CmdOption(names = Array("-s", "--start"), description = "Enable the start flag for the next bundle")
-      def nextBundleStart(): Unit = {
-        bundleOptions = bundleOptions.copy(start = true)
-      }
-
-      @CmdOption(names = Array("-l", "--bundle-start-level"), args = Array("level"), description = "Set the start level for the next bundle")
-      def nextBundleStartLevel(startLevel: Int): Unit = {
-        bundleOptions = bundleOptions.copy(startLevel = Option(startLevel))
-      }
-
-      @CmdOption(names = Array("-p", "--profile"), args = Array("name", "version"), description = "Set the name of this profile")
-      def setProfile(name: String, version: String): Unit = {
-        profile = Option(name -> version)
-      }
-
-      @CmdOption(args = Array("URL"),
-        description = "Add a bundle from the given {0} (partial URLs are supported if a base URL is defined")
-      def addBundle(url: String): Unit = {
-        val bundleUrl = s"${baseUrl.map(_ + "/").getOrElse("")}${url}"
-        // bundleOptions.copy(url = Option(bundleUrl)).toConfig()
-
-        bundleOptions.startLevel.map(l => "startLevel" -> l)
-        bundleOptions
-
-        bundleOptions = Options()
-      }
-
-    }
-
   }
 
   def main(args: Array[String]): Unit = {
