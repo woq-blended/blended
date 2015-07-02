623d999a12d245c19690650ece4b85a4b771713f
Andreas Gies <andreas@wayofquality.de>
2015-07-02 11:57:03 +0200

    Adding HHTP Service to demo container.

--- blended-launcher/run.sh
+++ blended-launcher/run.sh
@@ -2,7 +2,8 @@
 
 TOOLDIR="$(dirname "$0")"
 
-REPO="${TOOLDIR}/../../mvn/m2repo"
+#REPO="${TOOLDIR}/../../mvn/m2repo"
+REPO="/Users/andreas/.m2/repository"
 
 RETVAL=2
 
@@ -16,14 +17,19 @@ java\
 :"${REPO}/de/tototec/de.tototec.cmdoption/0.4.2/de.tototec.cmdoption-0.4.2.jar"\
 :"${REPO}/org/scala-lang/scala-library/2.10.5/scala-library-2.10.5.jar"\
 :"${REPO}/org/osgi/org.osgi.core/5.0.0/org.osgi.core-5.0.0.jar"\
-:"${REPO}/org/slf4j/slf4j-api/1.7.2/slf4j-api-1.7.2.jar"\
 :"${REPO}/ch/qos/logback/logback-core/1.1.3/logback-core-1.1.3.jar"\
 :"${REPO}/ch/qos/logback/logback-classic/1.1.3/logback-classic-1.1.3.jar"\
 :"${REPO}/com/typesafe/config/1.2.0/config-1.2.0.jar"\
 :"${REPO}/commons-daemon/commons-daemon/1.0.15/commons-daemon-1.0.15.jar"\
+ -Dfelix.fileinstall.filter=.*cfg\
+ -Dfelix.fileinstall.dir=/Users/andreas/projects/blended/blended-launcher-demo/target/classes/etc\
+ -Dfelix.fileinstall.bundles.new.start=false\
+ -Dfelix.fileinstall.enableConfigSave=false\
+ -Dhawtio.authenticationEnabled=false\
  blended.launcher.Launcher\
  --framework-restart 0\
- --profile-lookup ${TOOLDIR}/launch-profile.conf\
- "$@" && RETVAL=$? || RETVAL=$?
+ "$@" && RETVAL=$? || RETVAL=$?‚
 
 done
+
+#:"${REPO}/org/slf4j/slf4j-api/1.7.2/slf4j-api-1.7.2.jar"\
