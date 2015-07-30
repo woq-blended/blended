a86c33d8d495463d0f044057c430199a340c88cf
Tobias Roeser <le.petit.fou@web.de>
2015-07-30 11:46:53 +0200

    Added assembly configuration to blended.launcher

---
+++ blended-launcher/src/main/runner/bin/launcher.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TOOLDIR="$(dirname $(readlink -f "$0"))/.."
+
+RETVAL=2
+
+if [ -n "$DEBUG_PORT" ] ; then
+  JAVA_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=${DEBUG_PORT},suspend=y ${JAVA_OPTS}"
+fi
+
+while [ "x$RETVAL" == "x2" ]; do
+
+java\
+ $JAVA_OPTS\
+ -cp\
+ "${TOOLDIR}/lib/*"\
+ blended.launcher.Launcher\
+ --framework-restart 0\
+ "$@" && RETVAL=$? || RETVAL=$?
+
+done
+
+unset TOOLDIR
+unset RETVAL
