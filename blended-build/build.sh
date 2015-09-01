bcf1bb2416daf4e7080c0eb6fc257edae2f57fdc
Tobias Roeser <le.petit.fou@web.de>
2015-09-01 14:33:51 +0200

    Fixed too long output in travis

--- blended-build/build.sh
+++ blended-build/build.sh
@@ -3,11 +3,27 @@ set -e
 
 MVN="$(readlink -f "$(dirname "$0")/mvnw")"
 
+LOG_COUNT=0
+
+function build {
+  if [ "$TRAVIS" = "true" ]; then
+    LOG_COUNT=$((1+${LOG_COUNT}))
+    LOG_FILE=build-${LOG_COUNT}.log
+    echo "$MVN $@ > ${LOG_FILE}"
+    "$MVN" "$@" > ${LOG_FILE} 2>&1 ; echo "...(last 1000 lines)..." ; tail -n 1000 ${LOG_FILE} ; test ${PIPESTATUS[0]} -eq 0
+  else
+    "$MVN" "$@"
+  fi
+}
+
 "$MVN" -version
 
 cd blended-parent
-"$MVN" clean install
+
+build clean install
+
 cd ..
-"$MVN" clean install -P parent 2>&1 | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
-"$MVN" clean install -P updater 2>&1 | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
-"$MVN" clean install -P build,assembly 2>&1 | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
+
+build clean install -P parent
+build clean install -P updater
+build clean install -P build,assembly
