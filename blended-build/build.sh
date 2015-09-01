917a2611648d1014660a383f8163f1276f256fd8
Tobias Roeser <le.petit.fou@web.de>
2015-09-01 09:55:54 +0200

    Next try to apply grep also in Travis env

--- blended-build/build.sh
+++ blended-build/build.sh
@@ -8,6 +8,6 @@ MVN="$(readlink -f "$(dirname "$0")/mvnw")"
 cd blended-parent
 "$MVN" clean install
 cd ..
-"$MVN" clean install -P parent | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
-"$MVN" clean install -P updater | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
-"$MVN" clean install -P build,assembly | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
+"$MVN" clean install -P parent 2>&1 | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
+"$MVN" clean install -P updater 2>&1 | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
+"$MVN" clean install -P build,assembly 2>&1 | grep -v "^([INFO].*|)Download" | grep -v "longer than 100" ; test ${PIPESTATUS[0]} -eq 0
