b043b874010d886d83388403b148d9157c3bfd8a
Tobias Roeser <le.petit.fou@web.de>
2016-04-11 12:48:19 +0200

    Ask Launched JVM for system properties.

---
+++ blended-launcher/src/main/scala/blended/launcher/internal/ARM.scala
@@ -0,0 +1,19 @@
+package blended.launcher.internal
+
+/**
+ * Created by lefou on 11.04.16.
+ */
+object ARM {
+
+  def using[C <: AutoCloseable, R](c: C)(f: C => R): R = {
+    try {
+      f(c)
+    } finally {
+      try {
+        c.close()
+      } catch {
+        case t: Throwable => t.addSuppressed(t)
+      }
+    }
+  }
+}
