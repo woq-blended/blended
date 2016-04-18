81a831d12bf4de708fb440c1a804f4a5311a8c89
Tobias Roeser <le.petit.fou@web.de>
2016-04-18 15:51:02 +0200

    Also source application_overlay.conf from profiles

--- blended-container-context/src/main/scala/blended/container/context/internal/ContainerContextImpl.scala
+++ blended-container-context/src/main/scala/blended/container/context/internal/ContainerContextImpl.scala
@@ -18,6 +18,11 @@ package blended.container.context.internal
 import java.io.{BufferedInputStream, File, FileInputStream, FileNotFoundException, FileOutputStream, IOException}
 import java.util.Properties
 import blended.container.context.ContainerContext
+import blended.launcher.BrandingProperties
+import blended.updater.config.LocalOverlays
+import blended.updater.config.OverlayRef
+import blended.updater.config.RuntimeConfig
+import com.typesafe.config.ConfigParseOptions
 import org.slf4j.LoggerFactory
 import scala.collection.JavaConverters._
 import com.typesafe.config.Config
@@ -29,6 +34,7 @@ object ContainerContextImpl {
 }
 
 class ContainerContextImpl() extends ContainerContext {
+
   import ContainerContextImpl._
 
   private[this] val log = LoggerFactory.getLogger(classOf[ContainerContextImpl])
@@ -44,21 +50,27 @@ class ContainerContextImpl() extends ContainerContext {
 
   override def getContainerDirectory(): String = containerDir
 
+  val brandingProperties: Map[String, String] = {
+    val props = (try {
+      import blended.launcher.runtime.Branding
+      // it is possible, that this optional class is not available at runtime,
+      // e.g. when started with another launcher
+      log.debug("About to read launcher branding properies")
+      Option(Branding.getProperties())
+    } catch {
+      case e: NoClassDefFoundError => None
+    }) getOrElse {
+      log.warn("Could not read launcher branding properies")
+      new Properties()
+    }
+    props.entrySet().asScala.map(e => e.getKey().toString() -> e.getValue().toString()).toMap
+  }
+
   private[this] val containerDir: String = {
 
-    val profileHome = (
-      try {
-        import blended.launcher.runtime.Branding
-        // it is possible, that this optional class is not available at runtime, 
-        // e.g. when started with another launcher
-        log.debug("About to read launcher branding properies")
-        Option(Branding.getProperties())
-      } catch {
-        case e: NoClassDefFoundError => None
-      }
-    ).flatMap(ps => Option(ps.getProperty("blended.updater.profile.dir"))).
-      orElse {
-        log.warn("Could not read the profile directory from read launcher branding properies")
+    val profileHome =
+      brandingProperties.get(RuntimeConfig.Properties.PROFILE_DIR) orElse {
+        log.warn("Could not read the profile directory from read launcher branding properties")
         None
       }
 
@@ -88,20 +100,53 @@ class ContainerContextImpl() extends ContainerContext {
     val sysProps = ConfigFactory.systemProperties()
     val envProps = ConfigFactory.systemEnvironment()
 
-    // TODO: also read overlay config
-    ConfigFactory.parseFile(
-      new File(getContainerConfigDirectory, "application.conf")
-    ).withFallback(sysProps).withFallback(envProps).resolve()
+    val branding = brandingProperties
+    val overlayConfig = branding.get(RuntimeConfig.Properties.PROFILE_DIR) match {
+      case Some(profileDir) =>
+        branding.get(RuntimeConfig.Properties.OVERLAYS) match {
+          case Some(overlays) =>
+            val overlayRefs = overlays.split("[,]").toList.map(_.split("[:]", 2)).flatMap {
+              case Array(n, v) => Some(OverlayRef(n, v))
+              case x =>
+                log.debug("Unsupported overlay: " + x.mkString(":"))
+                None
+            }.toSet
+            if (overlayRefs.isEmpty) None
+            else {
+              val dir = LocalOverlays.materializedDir(overlayRefs, new File(profileDir))
+              val confFile = new File(dir, "etc/application_overlay.conf")
+              if (confFile.exists()) {
+                log.debug(s"About to read extra application overlay override file: ${confFile}")
+                Some(confFile)
+              } else None
+            }
+          case _ => None
+        }
+      case _ => None
+    }
+
+    log.debug("Overlay config: {}", overlayConfig)
+
+    val config = overlayConfig match {
+      case Some(oc) => ConfigFactory.parseFile(oc, ConfigParseOptions.defaults().setAllowMissing(false))
+      case _ => ConfigFactory.empty()
+    }
+    config.withFallback(ConfigFactory.parseFile(
+      new File(getContainerConfigDirectory, "application.conf"), ConfigParseOptions.defaults().setAllowMissing(false)
+    )).
+      withFallback(sysProps).
+      withFallback(envProps).
+      resolve()
   }
-  
+
   @deprecated
   override def readConfig(configId: String): Properties = {
 
-    def resolveSystemProps(in : Properties) : Properties = {
+    def resolveSystemProps(in: Properties): Properties = {
 
       val result = new Properties()
 
-      in.propertyNames().asScala.foreach { k=>
+      in.propertyNames().asScala.foreach { k =>
         val key = k.toString()
         var value = in.getProperty(key)
 
@@ -109,7 +154,7 @@ class ContainerContextImpl() extends ContainerContext {
 
         regex.findAllIn(value).matchData.map(_.group(1)).foreach { m =>
           Option(System.getProperty(m)) match {
-            case Some(sysProp) =>  value = value.replaceFirst("\\$\\{" + m + "}", sysProp)
+            case Some(sysProp) => value = value.replaceFirst("\\$\\{" + m + "}", sysProp)
             case None =>
           }
         }
