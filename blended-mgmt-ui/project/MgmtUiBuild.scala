c74a39b49b10e639909a17de3552ad1c95dc6b07
Andreas Gies <andreas@wayofquality.de>
2016-06-07 15:18:36 +0200

    Starting ScalaJS project for UI.

---
+++ blended-mgmt-ui/project/MgmtUiBuild.scala
@@ -0,0 +1,9 @@
+import sbt._
+
+object MgmtUiBuild extends Build {
+
+  val appName = "blended.mgmt.ui"
+
+  lazy val root = project.in(file("."))
+
+}
---
+++ blended-mgmt-ui/src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,12 @@
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+
+class TutorialApp extends JSApp {
+
+  @JSExport
+  override def main(): Unit = {
+    println("Hello World!")
+  }
+}
