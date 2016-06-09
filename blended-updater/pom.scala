6ff2e96603d77cccd4f5971096de1058d82f191b
Tobias Roeser <le.petit.fou@web.de>
2016-06-09 14:05:36 +0200

    Migrated blended.updater to pom.scala

---
+++ blended-updater/pom.scala
@@ -0,0 +1,63 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include blended-build/build-common.scala
+
+Model(
+  blendedUpdater,
+  packaging = "bundle",
+  name = "${project.artifactId}",
+  description = "OSGi Updater",
+  parent = blendedParent,
+  dependencies = Seq(
+    orgOsgi,
+    domino,
+    akkaOsgi,
+    slf4j,
+    typesafeConfig,
+    blendedUpdaterConfig,
+    blendedLauncher,
+    blendedMgmtBase,
+    blendedContainerContext,
+    blendedAkka,
+    blendedSprayApi,
+    akkaTestkit % "test",
+    scalaTest % "test",
+    felixFramework % "test",
+    logbackClassic % "test",
+    akkaSlf4j % "test",
+    felixGogoRuntime % "test",
+    felixGogoShell % "test",
+    felixGogoCommand % "test",
+    felixFileinstall % "test",
+    mockitoAll % "test",
+    blendedTestSupport % "test"
+  ),
+  properties = Map(
+    "bundle.symbolicName" -> "${project.artifactId}",
+    "bundle.namespace" -> "${project.artifactId}"
+  ),
+  build = Build(
+    plugins = Seq(
+      Plugin(
+        "org.apache.felix" % "maven-bundle-plugin",
+        extensions = true
+      ),
+      Plugin(
+        "net.alchim31.maven" % "scala-maven-plugin"
+      ),
+      Plugin(
+        "org.scalatest" % "scalatest-maven-plugin"
+      )
+    )
+  ),
+    profiles = Seq(Profile(
+    id = "gen-pom",
+    build = Build(
+      plugins = Seq(
+        generatePomXml(phase = "validate")
+      )
+    )
+  )),
+  modelVersion = "4.0.0"
+)
