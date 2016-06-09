1ea2639c6a5c060829c8d073018d0c3a3599203e
Tobias Roeser <le.petit.fou@web.de>
2016-06-09 08:33:11 +0200

    Read DB initialzation from bundle config

--- blended-persistence-orient/src/main/scala/blended/persistence/orient/internal/OrientActivator.scala
+++ blended-persistence-orient/src/main/scala/blended/persistence/orient/internal/OrientActivator.scala
@@ -6,33 +6,71 @@ import com.orientechnologies.orient.core.db.OPartitionedDatabasePool
 
 import blended.persistence.orient.PersistenceService
 import domino.DominoActivator
+import java.io.File
+import blended.domino.TypesafeConfigWatching
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
 
-class OrientActivator() extends DominoActivator {
+class OrientActivator() extends DominoActivator with TypesafeConfigWatching {
 
   private[this] val log = LoggerFactory.getLogger(classOf[OrientActivator])
 
   whenBundleActive {
-    log.debug("About to start {}", classOf[OrientActivator])
+    log.debug("About to start {}", getClass())
 
-    val dbUrl = "plocal:tmp$blended"
-    val dbUserName = "admin"
-    val dbPassword = "admin"
-    val dbPool = new OPartitionedDatabasePool(dbUrl, dbUserName, dbPassword)
+    whenTypesafeConfigAvailable { (config, idService) =>
 
-    val db = dbPool.acquire()
-    try {
-      if (!db.exists()) {
-        db.create()
+      def getString(path: String): Option[String] = if (config.hasPath(path)) Some(config.getString(path)) else None
+
+      val dbPath = getString("dbPath")
+      val dbUrl = getString("dbUrl").orElse(dbPath.map(p => s"plocal:${p}"))
+      val dbUserName = getString("dbUserName")
+      val dbPassword = getString("dbPassword")
+
+      (dbUrl, dbUserName, dbPassword) match {
+        case (None, _, _) =>
+          sys.error("No 'dbUrl' defined in configuration. Cannot start persistence service")
+
+        case (_, None, _) =>
+          sys.error("No 'dbUserName' defined in configuration. Cannot start persistence service")
+
+        case (_, _, None) =>
+          sys.error("No 'dbPassword' defined in configuration. Cannot start persistence service")
+
+        case (Some(dbUrl), Some(dbUserName), Some(dbPassword)) =>
+          dbPath.foreach { dbPath =>
+            val f = new File(dbPath)
+            if (!f.exists()) {
+              log.debug("dbPath is defined but does not exists. About to create it")
+              new File(dbPath).mkdirs()
+            }
+          }
+
+          {
+            val db = new ODatabaseDocumentTx(dbUrl)
+            try {
+              if (!db.exists()) {
+                log.debug("Database does not exist. About to create it at URL: {}", dbUrl)
+                db.create()
+              }
+            } finally {
+              db.close()
+            }
+          }
+
+          val dbPool = new OPartitionedDatabasePool(dbUrl, dbUserName, dbPassword)
+
+          onStop {
+            log.debug("About to close database pool")
+            dbPool.close()
+          }
+
+          val orientExperimental = new PersistenceServiceOrientDb(dbPool)
+          orientExperimental.providesService[PersistenceService]
+
+          log.debug("Started {}", getClass())
       }
-    } finally {
-      db.close()
-    }
 
-    val orientExperimental = new PersistenceServiceOrientDb(dbPool)
-    orientExperimental.providesService[PersistenceService]
-
-    onStop {
-      dbPool.close()
     }
 
   }
