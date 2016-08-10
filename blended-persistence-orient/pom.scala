522243fc71030632b0e8f6e0d282f4b909b9b0b2
Tobias Roeser <le.petit.fou@web.de>
2016-08-10 20:54:08 +0200

    Removed obsolete symlinks and fixed relative include paths in pom's
    Also bumped version of polyglot-scala extenstion to 0.1.19.
    
    See also: https://github.com/takari/polyglot-maven/pull/98

--- blended-persistence-orient/pom.scala
+++ blended-persistence-orient/pom.scala
@@ -1,9 +1,9 @@
 import org.sonatype.maven.polyglot.scala.model._
 import scala.collection.immutable.Seq
 
-#include blended-build/build-common.scala
-#include blended-build/build-dependencies.scala
-#include blended-build/build-plugins.scala
+#include ../blended-build/build-common.scala
+#include ../blended-build/build-dependencies.scala
+#include ../blended-build/build-plugins.scala
 
 BlendedModel(
   blendedPersistenceOrient,
