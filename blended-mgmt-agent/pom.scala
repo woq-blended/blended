0e4c39ca9aefbce6aab6f6d09f51619ba6773f8b
Tobias Roeser <le.petit.fou@web.de>
2016-08-19 14:56:59 +0200

    Migrated blended.mgmt.agent

---
+++ blended-mgmt-agent/pom.scala
@@ -0,0 +1,27 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../blended-build/build-common.scala
+#include ../blended-build/build-dependencies.scala
+#include ../blended-build/build-plugins.scala
+
+BlendedModel(
+  blendedMgmtAgent,
+  packaging = "bundle",
+  description = "Bundle to regularly report monitoring information to a central container hosting the container registry.",
+  dependencies = Seq(
+    scalaLib % "provided",
+    blendedAkka,
+    blendedContainerRegistry,
+    blendedUpdaterConfig,
+    blendedSprayApi,
+    orgOsgi,
+    akkaOsgi,
+    akkaTestkit % "test",
+    scalaTest % "test"
+  ),
+  plugins = Seq(
+    mavenBundlePlugin,
+    scalaMavenPlugin
+  )
+)
