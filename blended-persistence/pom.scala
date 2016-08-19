0de70cc22be5cf7c2ed30ebb3717155bea6adbc8
Tobias Roeser <le.petit.fou@web.de>
2016-08-19 12:50:58 +0200

    Migrated blended.persistence

---
+++ blended-persistence/pom.scala
@@ -0,0 +1,28 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../blended-build/build-common.scala
+#include ../blended-build/build-dependencies.scala
+#include ../blended-build/build-plugins.scala
+
+BlendedModel(
+  blendedPersistence,
+  packaging = "bundle",
+  description = "Provide a technology agnostic persistence API with pluggable Data Objects defined in other bundles.",
+  dependencies = Seq(
+    scalaLib % "provided",
+    blendedAkka,
+    domino,
+    sprayJson,
+    slf4j,
+    scalaTest % "test",
+    blendedTestSupport % "test",
+    mockitoAll % "test",
+    slf4jLog4j12 % "test"
+  ),
+  plugins = Seq(
+    mavenBundlePlugin,
+    scalaMavenPlugin,
+    scalatestMavenPlugin
+  )
+)
