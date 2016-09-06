4c7fc6c9808d42885ebc0dd9cd95b72de388b67f
Tobias Roeser <le.petit.fou@web.de>
2016-09-06 23:19:23 +0200

    Partially cherry-picked polyglot-scala definition for blended.updater.config from scalajs branch

---
+++ blended-updater-config/pom.scala
@@ -0,0 +1,24 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../blended-build/build-common.scala
+#include ../blended-build/build-dependencies.scala
+#include ../blended-build/build-plugins.scala
+
+BlendedModel(
+  gav = blendedUpdaterConfig,
+  packaging = "bundle",
+  description = "Configurations for Updater and Launcher",
+  dependencies = Seq(
+    scalaLib % "provided",
+    typesafeConfig,
+    slf4j,
+    scalaTest % "test",
+    blendedTestSupport % "test"
+  ),
+  plugins = Seq(
+    mavenBundlePlugin,
+    scalaMavenPlugin,
+    scalatestMavenPlugin
+  )
+)
