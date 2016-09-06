52bee0bb39989e473918fb99e5212671d422ec3e
Tobias Roeser <le.petit.fou@web.de>
2016-09-06 14:48:29 +0200

    Migrated blended.domino to polyglot-scala

---
+++ blended-domino/pom.scala
@@ -0,0 +1,22 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../blended-build/build-common.scala
+#include ../blended-build/build-dependencies.scala
+#include ../blended-build/build-plugins.scala
+
+BlendedModel(
+  blendedDomino,
+  packaging = "bundle",
+  description = "Blended Domino extension for new Capsule scopes.",
+  dependencies = Seq(
+    domino,
+    typesafeConfig,
+    blendedContainerContext,
+    scalaLib
+  ),
+  plugins = Seq(
+    mavenBundlePlugin,
+    scalaMavenPlugin
+  )
+)
