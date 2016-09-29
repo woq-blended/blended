f5a9d707e8031337836e203433c31ba9805596ae
Andreas Gies <andreas@wayofquality.de>
2016-09-29 15:24:38 +0200

    Migrating blended.samples.camel to scala pom file.

---
+++ blended.samples/blended.samples.camel/pom.scala
@@ -0,0 +1,26 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../../blended.build/build-versions.scala
+#include ../../blended.build/build-common.scala
+#include ../../blended.build/build-dependencies.scala
+#include ../../blended.build/build-plugins.scala
+
+BlendedModel(
+  gav = blendedSamplesCamel,
+  packaging = "bundle",
+  description = "A sample camel route.",
+  dependencies = Seq(
+    domino,
+    camelCore,
+    camelJms,
+    geronimoJms11Spec,
+    slf4j
+  ),
+  build = Build(
+    plugins = Seq(
+      mavenBundlePlugin, 
+      scalaMavenPlugin
+    )
+  )
+)
