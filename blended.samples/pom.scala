af27fcb17c7f7e058199b2bf7ab0e081a71b6147
Andreas Gies <andreas@wayofquality.de>
2016-09-29 13:07:43 +0200

    Migrating samples reactor pom to scala.

---
+++ blended.samples/pom.scala
@@ -0,0 +1,22 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../blended.build/build-versions.scala
+#include ../blended.build/build-common.scala
+#include ../blended.build/build-dependencies.scala
+#include ../blended.build/build-plugins.scala
+
+BlendedModel(
+  gav = blendedSamplesReactor,
+  packaging = "pom",
+  profiles = Seq(
+    Profile(
+      id = "build",
+      modules = Seq(
+        "blended-samples-camel",
+        "blended-samples-jms",
+        "blended-samples-spray-helloworld"
+      )
+    )
+  )
+)
