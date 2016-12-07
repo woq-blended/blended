7ce3f105c95c233b71020100658ba63d9ca48c4d
Tobias Roeser <le.petit.fou@web.de>
2016-12-07 11:43:43 +0100

    Removed upickle code

--- blended.updater.config/src/shared/scala/blended/updater/config/OverlayState.scala
+++ blended.updater.config/src/shared/scala/blended/updater/config/OverlayState.scala
@@ -1,7 +1,5 @@
 package blended.updater.config
 
-import upickle.Js
-
 sealed trait OverlayState {
   val state: String
 }
@@ -19,17 +17,6 @@ object OverlayState {
   }
   final case object Pending extends OverlayState {
     override val state: String = "pending"
-  }
-
-  implicit val os2writer = upickle.default.Writer[OverlayState]{
-    case state => Js.Str(state.state)
-  }
-
-  implicit val os2Reader = upickle.default.Reader[OverlayState]{
-    case Js.Str(Active.state) => Active
-    case Js.Str(Valid.state) => Valid
-    case Js.Str(Invalid.state) => Invalid
-    case Js.Str(Pending.state) => Pending
   }
 
 }
--- blended.updater.config/src/shared/scala/blended/updater/config/UpdateAction.scala
+++ blended.updater.config/src/shared/scala/blended/updater/config/UpdateAction.scala
@@ -1,30 +1,10 @@
 package blended.updater.config
 
-import upickle.Js
-import upickle.default._
-
 object UpdateAction {
   val KindAddOverlayConfig = classOf[AddOverlayConfig].getSimpleName()
   val KindAddRuntimeConfig = classOf[AddRuntimeConfig].getSimpleName()
   val KindStageProfile = classOf[StageProfile].getSimpleName()
   val KindActivateProfile = classOf[ActivateProfile].getSimpleName()
-
-  implicit val updateActionWriter = Writer[UpdateAction] {
-    case ua => ua match {
-      case a: StageProfile => writeJs(a)
-      case a: ActivateProfile => writeJs(a)
-      case a: AddRuntimeConfig => writeJs(a)
-      case a: AddOverlayConfig => writeJs(a)
-      case _ => throw new SerializationException(s"Could not write object ${ua}")
-    }
-  }
-
-  implicit val updateActionReader = Reader[UpdateAction] {
-    case Js.Obj(obj) =>
-      println(obj)
-      ActivateProfile("foo", "bar", List.empty)
-    case _=> throw new SerializationException(s"Could not deserialize UpdateAction")
-  }
 }
 
 sealed trait UpdateAction
