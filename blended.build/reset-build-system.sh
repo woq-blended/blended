70283e2e2d13814b324678cfc7ef858d7177b8be
Andreas Gieswq <andreas@wayofquality.de>
2016-12-08 15:10:31 +0100

    adding build helper scripts.

---
+++ blended.build/reset-build-system.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -Rf ~/.m2/repository
+rm -Rf ~/.ivy2/cache
+
+ps -ef | grep zinc | grep java | awk {'print "kill -9 " $2'} | sh
+wget http://downloads.typesafe.com/zinc/0.3.11/zinc-0.3.11.tgz
+tar -xzf zinc-0.3.11.tgz
+sh ./zinc-0.3.11/bin/zinc -start
+
+
