9127a8bb0d9f47f896446eb1f764feebce407412
Andreas Gieswq <andreas@wayofquality.de>
2016-12-08 15:29:29 +0100

    Adding scala home to zinc start:

--- blended.build/reset-build-system.sh
+++ blended.build/reset-build-system.sh
@@ -6,6 +6,6 @@ rm -Rf ~/.ivy2/cache
 ps -ef | grep zinc | grep java | awk {'print "kill -9 " $2'} | sh
 wget http://downloads.typesafe.com/zinc/0.3.11/zinc-0.3.11.tgz
 tar -xzf zinc-0.3.11.tgz
-sh ./zinc-0.3.11/bin/zinc -start
+sh ./zinc-0.3.11/bin/zinc -start -scala-home /opt/scala
 
 
