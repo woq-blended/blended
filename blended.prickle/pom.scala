ef176b96ee2f81910470f82d29c13f5620644f27
Tobias Roeser <le.petit.fou@web.de>
2016-12-08 16:08:28 +0100

    Added new OSGi wrapper project for prickle and microjson

---
+++ blended.prickle/pom.scala
@@ -0,0 +1,32 @@
+import org.sonatype.maven.polyglot.scala.model._
+import scala.collection.immutable.Seq
+
+#include ../blended.build/build-versions.scala
+#include ../blended.build/build-dependencies.scala
+#include ../blended.build/build-plugins.scala
+#include ../blended.build/build-common.scala
+
+BlendedModel(
+  blendedPrickle,
+  packaging = "bundle",
+  description = "Wrapper for Prickle and mircojson",
+  dependencies = Seq(
+    prickle,
+    microjson
+  ),
+  plugins = Seq(
+    Plugin(
+      gav = mavenBundlePlugin.gav,
+      extensions = true,
+      configuration = Config(
+        instructions = new Config(Seq(
+          "Bundle-Version" -> blendedPrickle.version,
+          "Bundle-SymbolicName" -> Option(blendedPrickle.artifactId),
+          "Import-Package" -> Option("scala.*;version=\"[" + scalaVersion.binaryVersion + "," + scalaVersion.binaryVersion + ".50)\"),prickle,microjson,*"),
+          "Embed-Dependency" -> Option(prickle.artifactId + ";inline=true," + microjson.artifactId + ";inline=true"),
+          "Export-Package" -> Option("prickle;version=\"" + prickle.version.get + "\",microjson;version=\"" + microjson.version.get + "\"")
+        ))
+      )
+    )
+  )
+)
