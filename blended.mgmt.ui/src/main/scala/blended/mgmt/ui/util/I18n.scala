2685836d1ce7e5b07c023e3112db74dcc413bb77
Tobias Roeser <le.petit.fou@web.de>
2017-01-16 11:48:22 +0100

    Added I18n classes

---
+++ blended.mgmt.ui/src/main/scala/blended/mgmt/ui/util/I18n.scala
@@ -0,0 +1,91 @@
+package blended.mgmt.ui.util
+
+import java.util.Locale
+import scala.reflect.ClassTag
+import scala.reflect.classTag
+import java.util.MissingResourceException
+import java.util.ResourceBundle
+import java.text.MessageFormat
+import java.util.jar.JarInputStream
+import java.io.File
+import java.io.FileInputStream
+import scala.collection.mutable.WeakHashMap
+import scala.util.Try
+import java.util.regex.Matcher
+
+trait I18nMarker {
+  @inline def marktr(msgid: String): String = msgid
+  @inline def marktrc(context: String, msgid: String): String = msgid
+  @inline def notr(msgid: String, params: Any*): String = params match {
+    case Seq() => msgid
+    case _ => 
+      val idx = 0
+      var msg = msgid
+      val args = params.map(_.toString).foreach { arg =>
+        msg = msg.replaceAll("\\Q{" + idx + "}\\E", Matcher.quoteReplacement(arg))
+      }
+      msg
+  }
+}
+
+trait I18n extends I18nMarker {
+  def tr(msgid: String, params: Any*): String
+  def trn(msgid: String, msgidPlural: String, n: Long, params: Any*): String
+  def trc(context: String, msgid: String, params: Any*): String
+  def trcn(context: String, msgid: String, msgidPlural: String, n: Long, params: Any*): String
+  def locale: String
+  def preparetr(msgid: String, params: Any*): PreparedI18n
+}
+
+trait PreparedI18n {
+  def tr: String
+  def notr: String
+}
+
+object I18n extends I18nMarker {
+
+  private[this] val log = Logger("blended.mgmt.ui.util.I18n")
+
+  var missingTranslationDecorator: Option[String => String] = None
+
+  def apply(): I18n = apply("")
+  def apply(locale: String): I18n = {
+    new I18nImpl(locale, missingTranslationDecorator)
+  }
+}
+
+class I18nImpl(override val locale: String, missingTranslationDecorator: Option[String => String]) extends I18n {
+
+  private[this] lazy val log = Logger("blended.mgmt.ui.util.I18nImpl")
+
+  override def toString = "I18nImpl(locale=" + locale + ")"
+
+  private[this] def translate(msgid: String): String = {
+    // TODO: make a message lookup
+    log.trace("Could not lookup msgid \"" + msgid + "\" for locale \"" + locale + "\"")
+    missingTranslationDecorator match {
+      case None => I18n.missingTranslationDecorator match {
+        case None => msgid
+        case Some(miss) => miss(msgid)
+      }
+      case Some(miss) => miss(msgid)
+    }
+  }
+
+  override def tr(msgid: String, params: Any*): String = notr(translate(msgid), params: _*)
+
+  override def trn(msgid: String, msgidPlural: String, n: Long, params: Any*): String = tr(n match {
+    case 1 => msgid
+    case _ => msgidPlural
+  }, params: _*)
+
+  override def trc(context: String, msgid: String, params: Any*): String = tr(msgid, params: _*)
+
+  override def trcn(context: String, msgid: String, msgidPlural: String, n: Long, params: Any*): String = trn(msgid, msgidPlural, n, params: _*)
+
+  @inline override def preparetr(msgid: String, params: Any*): PreparedI18n = new PreparedI18n {
+    override def tr: String = I18nImpl.this.tr(msgid, params: _*)
+    override def notr: String = I18nImpl.this.notr(msgid, params: _*)
+  }
+
+}
