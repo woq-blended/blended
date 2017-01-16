f3744f67b238ec4bef1bb70d02dd256f2c0c1fb4
Tobias Roeser <le.petit.fou@web.de>
2017-01-16 11:48:02 +0100

    Added new Logger class

---
+++ blended.mgmt.ui/src/main/scala/blended/mgmt/ui/util/Logger.scala
@@ -0,0 +1,43 @@
+package blended.mgmt.ui.util
+
+trait Logger extends Serializable {
+  def error(msg: => String, throwable: Throwable = null)
+  def warn(msg: => String, throwable: Throwable = null)
+  def info(msg: => String, throwable: Throwable = null)
+  def debug(msg: => String, throwable: Throwable = null)
+  def trace(msg: => String, throwable: Throwable = null)
+}
+
+object Logger {
+
+  private[this] lazy val noOpLogger = new Logger {
+    override def error(msg: => String, throwable: Throwable) {}
+    override def warn(msg: => String, throwable: Throwable) {}
+    override def info(msg: => String, throwable: Throwable) {}
+    override def debug(msg: => String, throwable: Throwable) {}
+    override def trace(msg: => String, throwable: Throwable) {}
+  }
+
+  
+  class PrintlnLogger(className: String) extends Logger {
+    override def error(msg: => String, throwable: Throwable) = log("ERROR", msg, throwable)
+    override def warn(msg: => String, throwable: Throwable) = log("WARN", msg, throwable)
+    override def info(msg: => String, throwable: Throwable) = log("INFO", msg, throwable)
+    override def debug(msg: => String, throwable: Throwable) = log("DEBUG", msg, throwable)
+    override def trace(msg: => String, throwable: Throwable) = log("TRACE", msg, throwable)
+    def log(level: String, msg: => String, throwable: Throwable) = {
+      println(level + " " + className + ": " +  msg)
+      if(throwable != null) {
+        println(throwable)
+      }
+    }
+  }
+  
+  def apply(): Logger = apply("j")
+  def apply(className: String): Logger = {
+    // for now, delegate all log message to println
+    new PrintlnLogger(className)
+  }
+
+}
+
