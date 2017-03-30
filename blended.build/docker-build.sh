9f37e63c628d87ee1f53501cf82c2166d818b905
Andreas Gies <andreas@wayofquality.de>
2017-03-30 09:48:22 +0200

    First cut of docker based build

---
+++ blended.build/docker-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+set -e 
+
+export DOCKER_HOST=127.0.0.1
+export DOCKER_PORT=2375 
+
+source /home/blended/.nvm/nvm.sh
+nvm use 4.2.6
+
+node --version 
+
+nohup sudo /usr/bin/dockerd -H 127.0.0.1:2375 &> /tmp/docker.out &
+
+cd ~/project 
+
+mvn clean install -P build,docker,itest -Ddocker.host=$DOCKER_HOST -Ddocker.port=$DOCKER_PORT
