b02bb7dc0979b1ac2c44ece2a6a314971bb52e4d
Andreas Gies <andreas@wayofquality.de>
2017-03-30 15:38:06 +0200

    Refining docker build script for travis

--- blended.build/docker-build.sh
+++ blended.build/docker-build.sh
@@ -1,20 +1,18 @@
 #!/bin/bash 
 
 set -e 
+set -x
 
 export DOCKER_HOST=127.0.0.1
 export DOCKER_PORT=2375 
 
-source /home/blended/.nvm/nvm.sh
+source ~/.nvm/nvm.sh
 nvm use 4.2.6
 
 node --version 
 
-nohup sudo /usr/bin/dockerd -H 127.0.0.1:2375 &> /tmp/docker.out &
+# nohup sudo /usr/bin/dockerd -H 127.0.0.1:2375 &> /tmp/docker.out &
 
-cd ~/project 
+cd ~/project
 
-(mvn install -P build,docker,itest -Ddocker.host=$DOCKER_HOST -Ddocker.port=$DOCKER_PORT > /tmp/mvn.out 2>&1 &) ; $MVN_RC=$? 
-tail -f /tmp/mvn.out | grep -v -i "download" | grep -v -i "CheckForNull" | grep -v -i "longer than 100 characters"
-
-exit $MVN_RC
+mvn install -P build,docker,itest -Ddocker.host=$DOCKER_HOST -Ddocker.port=$DOCKER_PORT
