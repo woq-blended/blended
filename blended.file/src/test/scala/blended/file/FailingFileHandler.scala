a01815fa498085b5e51a0f6628f6a88ed14e91a3
Andreas Gies <andreas@wayofquality.de>
2017-05-11 20:13:33 +0200

    Working on File Service API implementation

--- blended.file/src/test/scala/blended/file/FailingFileHandler.scala
+++ blended.file/src/test/scala/blended/file/FailingFileHandler.scala
@@ -1,9 +1,9 @@
 package blended.file
-import java.io.InputStream
+import java.io.File
 
 class FailingFileHandler extends FilePollHandler {
 
   @throws[Throwable]
-  override def processFile(is: InputStream, props: Map[String, Object]): Unit =
+  override def processFile(f : File, props: Map[String, Object]): Unit =
     throw new Exception("Could not process !!")
 }
