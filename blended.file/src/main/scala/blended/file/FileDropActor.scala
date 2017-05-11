238f38bebf1d0bb192b2cec38aecacb59794d114
Andreas Gies <andreas@wayofquality.de>
2017-05-11 22:59:20 +0200

    First cut of FileDrop Actor with support for compression and Append

---
+++ blended.file/src/main/scala/blended/file/FileDropActor.scala
@@ -0,0 +1,161 @@
+package blended.file
+
+import java.io._
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.zip.GZIPInputStream
+
+import akka.actor.{Actor, ActorLogging}
+import blended.util.StreamCopySupport
+
+import scala.util.control.NonFatal
+
+case class FileDropCommand(
+  content: Array[Byte],
+  directory: String,
+  fileName: String,
+  compressed: Boolean = false,
+  append: Boolean = false,
+  timestamp: Long = System.currentTimeMillis()
+) {
+  override def toString: String = {
+
+    val ts = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss:SSS").format(new Date(timestamp))
+    s"FileDropCommand(dir = [$directory], fileName = [$fileName], compressed = $compressed, append = $append, timestamp = [$ts], content-size = ${content.size})"
+
+  }
+}
+
+case class FileDropResult(cmd: FileDropCommand, success: Boolean)
+
+class FileDropActor extends Actor with ActorLogging {
+
+  def checkDirectory(dir: File) : Boolean = {
+
+    if (!dir.exists()) {
+      log.debug(s"Creating directory [${dir.getAbsolutePath()}]")
+      dir.mkdirs()
+    }
+
+    (dir.exists() && dir.isDirectory() && dir.canWrite())
+  }
+
+  def finalFile(cmd: FileDropCommand) : File = {
+
+    val file = new File(cmd.directory, cmd.fileName)
+
+    if (!cmd.append) {
+      if (file.exists()) {
+        // In case we need to generate a new file name
+        new File(cmd.directory, cmd.fileName.lastIndexOf('.') match {
+          case -1 => s"dup_${System.currentTimeMillis()}_${cmd.fileName}"
+          case pos => s"${cmd.fileName.substring(0, pos)}.dup_${System.currentTimeMillis()}${cmd.fileName.substring(pos)}"
+        })
+      } else {
+        // In case we do not append and we can generate a new file
+        file
+      }
+    } else {
+      // In case we append the content to a file, we keep the same final file name
+      file
+    }
+  }
+
+  // A temp file is only created when we need to append to an existing file.
+  def tmpFile(cmd: FileDropCommand) : Option[File] = {
+    if (cmd.append) {
+      val file = finalFile(cmd)
+      if (file.exists()) {
+        val tmpName = s"${cmd.fileName}.${cmd.timestamp}.tmp"
+        val tmpFile = new File(cmd.directory, tmpName)
+        log.debug(s"Creating temporary file [${tmpFile.getAbsolutePath()}]")
+        file.renameTo(tmpFile)
+        Some(tmpFile)
+      } else {
+        None
+      }
+    } else {
+      None
+    }
+  }
+
+  // The outfile is the file that will temporarily hold the final content
+  def outFile(cmd: FileDropCommand) : File =
+    new File(cmd.directory, s"${cmd.fileName}.${cmd.timestamp}.out")
+
+  def prepareOutputStream(cmd: FileDropCommand, tmpFile: Option[File]) : OutputStream = {
+
+    val os = new FileOutputStream(outFile(cmd))
+
+    if (cmd.append) {
+      tmpFile match {
+        case None =>
+        case Some(f) =>
+          log.debug(s"Copying original content before appending into file ${f.getAbsolutePath()}")
+          val tmpIn = new FileInputStream(f)
+          StreamCopySupport.copyStream(tmpIn, os)
+          tmpIn.close()
+      }
+    }
+
+    os
+  }
+
+  override def receive: Receive = {
+
+    case cmd : FileDropCommand =>
+
+      val requestor = sender()
+      val outdir = new File(cmd.directory)
+
+      var os : Option[OutputStream] = None
+      var rawIs : Option[InputStream] = None
+      var tf : Option[File] = None
+
+      if (checkDirectory(outdir)) {
+
+        val is : InputStream = cmd.compressed match {
+          case true =>
+            log.debug(s"Writing content with compression to ${outFile(cmd)}")
+            val bis = new ByteArrayInputStream(cmd.content)
+            rawIs = Some(bis)
+            new GZIPInputStream(bis)
+          case false =>
+            log.debug(s"Writing content without compression to ${outFile(cmd)}")
+            new ByteArrayInputStream(cmd.content)
+        }
+
+        try {
+          tf = tmpFile(cmd)
+          os = Some(prepareOutputStream(cmd, tf))
+
+          os.foreach{ s =>
+            StreamCopySupport.copyStream(is, s)
+            s.close()
+          }
+
+          val ff = finalFile(cmd)
+          outFile(cmd).renameTo(ff)
+          tf.foreach{ f => f.delete() }
+
+          log.info(s"Successfully executed [$cmd] and created file [${ff.getAbsolutePath()}]")
+
+          requestor ! FileDropResult(cmd, true)
+        } catch {
+          case NonFatal(t) =>
+            log.warning(s"Error executing ${cmd}: ${t.getMessage()}")
+
+            tf.foreach { f => f.renameTo(new File(cmd.directory, cmd.fileName)) }
+            outFile(cmd).delete()
+
+            requestor ! FileDropResult(cmd, false)
+        } finally {
+          rawIs.foreach(_.close())
+          is.close()
+        }
+      } else {
+        log.warning(s"The directory [${outdir.getAbsolutePath()}] does not exist or is not writable.")
+        requestor ! FileDropResult(cmd, false)
+      }
+  }
+}
---
+++ blended.file/src/test/scala/blended/file/FileDropSpec.scala
@@ -0,0 +1,41 @@
+package blended.file
+
+import java.io.File
+
+import akka.actor.Props
+import akka.testkit.TestProbe
+import blended.testsupport.TestActorSys
+import blended.util.FileHelper
+import org.scalatest.{FreeSpec, Matchers}
+
+class FileDropSpec extends FreeSpec with Matchers {
+
+  "The FileDropActor should" - {
+
+    "drop an uncompressed file into a given directory" in TestActorSys { testkit =>
+
+      implicit val system = testkit.system
+
+      val cmd = FileDropCommand(
+        content = "Hallo Andreas".getBytes(),
+        directory = System.getProperty("projectTestOutput") + "/drop",
+        fileName = "test.txt"
+      )
+
+      val probe = TestProbe()
+
+      system.actorOf(Props[FileDropActor]).tell(cmd, probe.ref)
+
+      probe.expectMsg(FileDropResult(cmd, true))
+
+      val f : File = new File(cmd.directory, cmd.fileName)
+
+      f.exists() should be (true)
+
+      val content = FileHelper.readFile(f.getAbsolutePath())
+
+      content should be (cmd.content)
+    }
+  }
+
+}
