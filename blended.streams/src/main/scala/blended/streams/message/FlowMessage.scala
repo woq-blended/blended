209f07c719412f84020f74f353a15949a46f5dc8
Andreas Gies <andreas@wayofquality.de>
2017-07-26 12:10:38 +0200

    Working on FlowMessageDefinition

--- blended.streams/src/main/scala/blended/streams/message/FlowMessage.scala
+++ blended.streams/src/main/scala/blended/streams/message/FlowMessage.scala
@@ -3,9 +3,10 @@ package blended.streams.message
 import akka.NotUsed
 import akka.util.ByteString
 
+import scala.util.Try
+
 sealed trait MsgProperty {
   def value() : Any
-
   override def toString: String = value().toString()
 }
 
@@ -13,6 +14,10 @@ case class StringMsgProperty(value: String) extends MsgProperty
 case class IntMsgProperty(value: Int) extends MsgProperty
 case class LongMsgProperty(value: Long) extends MsgProperty
 case class BooleanMsgProperty(value : Boolean) extends MsgProperty
+case class ByteMsgProperty(value : Byte) extends MsgProperty
+case class ShortMsgProperty(value : Short) extends MsgProperty
+case class FloatMsgProperty(value: Float) extends MsgProperty
+case class DoubleMsgProperty(value: Double) extends MsgProperty
 
 case object MsgProperty {
 
@@ -22,17 +27,43 @@ case object MsgProperty {
   implicit def intToProp(i : Int) : MsgProperty = IntMsgProperty(i)
   implicit def longToProp(l : Long) : MsgProperty = LongMsgProperty(l)
   implicit def boolToProp(b : Boolean) : MsgProperty = BooleanMsgProperty(b)
+  implicit def byteToProp(b : Byte) : MsgProperty = ByteMsgProperty(b)
+  implicit def shortToProp(s : Short) : MsgProperty = ShortMsgProperty(s)
+  implicit def floatToProp(f : Float) : MsgProperty = FloatMsgProperty(f)
+  implicit def doubleToProp(d : Double) : MsgProperty = DoubleMsgProperty(d)
 
   def apply(s : String) : MsgProperty = new StringMsgProperty(s)
   def apply(i : Int) : MsgProperty = new IntMsgProperty(i)
   def apply(l : Long) : MsgProperty = new LongMsgProperty(l)
   def apply(b : Boolean) : MsgProperty = new BooleanMsgProperty(b)
+  def apply(b : Byte) : MsgProperty = new ByteMsgProperty(b)
+  def apply(s : Short) : MsgProperty = new ShortMsgProperty(s)
+  def apply(f : Float) : MsgProperty = new FloatMsgProperty(f)
+  def apply(d : Double) : MsgProperty = new DoubleMsgProperty(d)
+
+  def lift(o : AnyRef) : Try[MsgProperty] = Try {
+    o match {
+      case s: String => apply(s)
+      case i: Integer => apply(i)
+      case l: java.lang.Long => apply(l)
+      case b: java.lang.Boolean => apply(b)
+      case b: java.lang.Byte => apply(b)
+      case s: java.lang.Short => apply(s)
+      case f: java.lang.Float => apply(f)
+      case d: java.lang.Double => apply(d)
+      case _ => throw new IllegalArgumentException("Unsupported Msg Property type")
+    }
+  }
 
   def unapply(p: MsgProperty): Any = p match {
     case s : StringMsgProperty => s.value
     case i : IntMsgProperty => i.value
     case l : LongMsgProperty => l.value
     case b : BooleanMsgProperty => b.value
+    case b : ByteMsgProperty => b.value
+    case s : ShortMsgProperty => s.value
+    case f : FloatMsgProperty => f.value
+    case d : DoubleMsgProperty => d.value
   }
 }
 
