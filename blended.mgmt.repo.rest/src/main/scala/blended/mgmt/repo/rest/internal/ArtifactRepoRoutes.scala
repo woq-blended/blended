96ea1143bc63b57cdcee970e41a23d53d4bfa358
Tobias Roeser <le.petit.fou@web.de>
2017-08-09 14:12:40 +0200

    Logging and explicit return type

--- blended.mgmt.repo.rest/src/main/scala/blended/mgmt/repo/rest/internal/ArtifactRepoRoutes.scala
+++ blended.mgmt.repo.rest/src/main/scala/blended/mgmt/repo/rest/internal/ArtifactRepoRoutes.scala
@@ -5,16 +5,21 @@ import blended.spray.BlendedHttpRoute
 import org.slf4j.LoggerFactory
 import spray.http.StatusCodes
 import blended.security.spray.BlendedSecuredRoute
+import spray.routing.Route
 
 trait ArtifactRepoRoutes
     extends BlendedHttpRoute
-    with BlendedSecuredRoute {
+    with BlendedSecuredRoute { self =>
 
-  private[this] val log = LoggerFactory.getLogger(classOf[ArtifactRepoRoutes])
+  private[this] val log = {
+    val log = LoggerFactory.getLogger(classOf[ArtifactRepoRoutes])
+    log.debug("Creating {}", self)
+    log
+  }
 
   protected def artifactRepo: ArtifactRepo
 
-  override val httpRoute = get { r =>
+  override val httpRoute: Route = get { r =>
     requirePermission("artifact:get") {
       val path = r.unmatchedPath.toString()
       log.debug("Request for path: {}", path)
