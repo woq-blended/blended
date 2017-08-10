f637e4baac47045488774e116154005b830a6b9f
Tobias Roeser <le.petit.fou@web.de>
2017-08-10 11:07:41 +0200

    Refactored file repo rest module
    It now handles all repos with one route, and also only one actor.
    
    Doesn't work correctly, though.

--- blended.mgmt.repo.rest/src/main/scala/blended/mgmt/repo/rest/internal/ArtifactRepoRoutes.scala
+++ blended.mgmt.repo.rest/src/main/scala/blended/mgmt/repo/rest/internal/ArtifactRepoRoutes.scala
@@ -9,25 +9,33 @@ import spray.routing.Route
 
 trait ArtifactRepoRoutes
     extends BlendedHttpRoute
-    with BlendedSecuredRoute { self =>
+    with BlendedSecuredRoute {
 
-  private[this] val log = {
-    val log = LoggerFactory.getLogger(classOf[ArtifactRepoRoutes])
-    log.debug("Creating {}", self)
-    log
-  }
+  private[this] val log = LoggerFactory.getLogger(classOf[ArtifactRepoRoutes])
 
-  protected def artifactRepo: ArtifactRepo
+  protected def artifactRepos: List[ArtifactRepo]
 
-  override val httpRoute: Route = get { r =>
-    requirePermission("artifact:get") {
-      val path = r.unmatchedPath.toString()
-      log.debug("Request for path: {}", path)
-
-      artifactRepo.findFile(path) match {
-        case Some(file) => getFromFile(file)
-        case None => complete(StatusCodes.NotFound)
-      }
+  override val httpRoute: Route = pathPrefix(Segment) { id =>
+    artifactRepos.find(a => a.repoId == id) match {
+      case None =>
+        log.debug("No repository with id: {}", id)
+        complete(StatusCodes.NotFound)
+      case Some(repo) =>
+        log.debug("Selected repository with id: {}", id)
+        get { r =>
+          requirePermission("artifact:get") {
+            val path = r.unmatchedPath.toString()
+            log.debug("Request for path: {}", path)
+            repo.findFile(path) match {
+              case Some(file) => getFromFile(file)
+              case None =>
+                repo.findFiles(path) match {
+                  case Iterator.empty => complete(StatusCodes.NotFound)
+                  case files => complete("<ul>" + files.mkString("<li>", "</li>\n<li>", "</li>") + "</ul>")
+                }
+            }
+          }
+        }
     }
   }
 
