53c635ec4febd5ee5fda36ca257005fda7cfbd40
Andreas Gies <andreas@wayofquality.de>
2017-08-22 14:52:26 +0200

    adding build script to build scalajs-react-components

---
+++ blended.build/buildScalaJSReactComponents.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+mkdir -p "../3rdparty"
+
+DIR=scalajs-react-components
+BRANCH=upgrade-1.1.0
+VERSION=0.7.0
+
+(
+  cd ../3rdparty &&
+  rm -rf "${DIR}" &&
+  git clone https://github.com/woq-blended/scalajs-react-components.git "${DIR}" &&
+  cd "${DIR}" &&
+  git checkout "${BRANCH}" &&
+  sbt -batch clean "+ publishLocal"
+)
