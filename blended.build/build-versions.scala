711bb7faacbdfbca0196d9daee978ef8acff8a7a
Tobias Roeser <le.petit.fou@web.de>
2017-08-23 13:44:00 +0200

    Some dependency version bumps: scalatest, akka, spray, prickle

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -9,10 +9,10 @@ object BlendedVersions {
   val scalaVersion = "2.11.11"
   val scalaJsVersion = "0.6.19"
 
-  val scalaTestVersion = "3.0.3"
+  val scalaTestVersion = "3.0.4"
 
   val activeMqVersion = "5.14.5"
-  val akkaVersion = "2.5.2"
+  val akkaVersion = "2.5.4"
   val apacheShiroVersion = "1.2.4"
 
   val camelVersion = "2.17.3"
@@ -24,12 +24,12 @@ object BlendedVersions {
   val parboiledVersion = "1.1.6"
 
   val slf4jVersion = "1.7.25"
-  val sprayVersion = "1.3.2"
+  val sprayVersion = "1.3.3"
   // from apache servicemix
   val springVersion = "3.2.18.RELEASE_1"
 
   val paxWeb = "3.1.6"
-  val prickle = "1.1.13"
+  val prickle = "1.1.14"
 
   val xbean = "3.16"
 
