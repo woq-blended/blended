3e95dd496074e689a17d816a3e201ebc59f05969
Tobias Roeser <le.petit.fou@web.de>
2017-09-19 15:02:15 +0200

    Install jar to maven repo too

--- blended.build/buildScalaJSReactComponents.sh
+++ blended.build/buildScalaJSReactComponents.sh
@@ -13,4 +13,6 @@ VERSION=0.8.1
   cd "${DIR}" &&
   git checkout "${BRANCH}" &&
   sbt -batch clean "+ publishLocal"
+  mvn install:install-file -DartifactId=scalajs-react-components_sjs0.6_2.11 -DgroupId=com.olvind -Dversion=${VERSION} -DgeneratePom=true -Dfile=core/target/scala-2.11/scalajs-react-components_sjs0.6_2.11-${VERSION}.jar -Dpackaging=jar
+  mvn install:install-file -DartifactId=scalajs-react-components_sjs0.6_2.12 -DgroupId=com.olvind -Dversion=${VERSION} -DgeneratePom=true -Dfile=core/target/scala-2.12/scalajs-react-components_sjs0.6_2.12-${VERSION}.jar -Dpackaging=jar
 )
