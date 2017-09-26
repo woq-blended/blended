431338c75e4560d50483975219f696ed466c3adb
Tobias Roeser <le.petit.fou@web.de>
2017-09-26 15:53:52 +0200

    Added support for dedicated https urls in mgmt ui

--- blended.mgmt.ui/src/main/scala/blended/mgmt/ui/ConsoleSettings.scala
+++ blended.mgmt.ui/src/main/scala/blended/mgmt/ui/ConsoleSettings.scala
@@ -1,10 +1,20 @@
 package blended.mgmt.ui
 
+import scala.scalajs.js
+import blended.mgmt.ui.util.Logger
+
 object ConsoleSettings {
-  val mgmtUrl = "http://root:mysecret@mgmt:9191/mgmt"
-  val containerDataUrl = mgmtUrl + "/container"
-  val profilesUrl = mgmtUrl + "/profiles"
-  val runtimeConfigsUrl = mgmtUrl + "/runtimeConfig"
-  val overlayConfigUrl = mgmtUrl + "/overlayConfig"
-  val rolloutProfile: String = mgmtUrl + "/rollout/profile"
+  def isSsl = {
+    val prot = js.Dynamic.global.window.location.protocol.toString
+    Logger[ConsoleSettings.type].debug("protocol: " + prot)
+    prot == "https:"
+  }
+  def mgmtUrl =
+    if (isSsl) "https://root:mysecret@mgmt:9443/mgmt"
+    else "http://root:mysecret@mgmt:9191/mgmt"
+  def containerDataUrl = mgmtUrl + "/container"
+  def profilesUrl = mgmtUrl + "/profiles"
+  def runtimeConfigsUrl = mgmtUrl + "/runtimeConfig"
+  def overlayConfigUrl = mgmtUrl + "/overlayConfig"
+  def rolloutProfile: String = mgmtUrl + "/rollout/profile"
 }
