966f8b19c04077c1f60ff927a8950a9d681a4ee7
Andreas Gies <andreas@wayofquality.de>
2017-12-09 21:48:19 +0100

    After releasing 2.3.0-RC8

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.3.0-RC8"
+  val blendedVersion = "2.3.0-RC9-SNAPSHOT"
 
   val javaVersion = "1.8"
 
