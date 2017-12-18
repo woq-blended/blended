000f575f17b021a6f6b616911e386a11845d1a5e
Andreas Gies <andreas@wayofquality.de>
2017-12-18 20:08:34 +0100

    Releasing 2.3.0-RC9

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.3.0-RC9-SNAPSHOT"
+  val blendedVersion = "2.3.0-RC9"
 
   val javaVersion = "1.8"
 
