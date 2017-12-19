82b2efb62c899f11d0cd49ef62bdc0b86bcec81e
Andreas Gies <andreas@wayofquality.de>
2017-12-19 23:15:15 +0100

    After releasing 2.3.0-RC10

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.3.0-RC10"
+  val blendedVersion = "2.3.0-RC11-SNAPSHOT"
 
   val javaVersion = "1.8"
 
