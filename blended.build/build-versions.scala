7a66de3b861670dae6c62a8c01171bfb249f8238
Andreas Gies <andreas@wayofquality.de>
2017-12-21 00:29:59 +0100

    Releasing 2.3.0-RC11

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.3.0-RC11-SNAPSHOT"
+  val blendedVersion = "2.3.0-RC11"
 
   val javaVersion = "1.8"
 
