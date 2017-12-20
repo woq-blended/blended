a3e8ca84171512962967843d46d24b5c563efabc
Tobias Roeser <le.petit.fou@web.de>
2017-12-20 15:06:05 +0100

    Improved duplication removal in feature dependencies

--- blended.build/build-common.scala
+++ blended.build/build-common.scala
@@ -365,7 +365,17 @@ case class FeatureBundle(
 // Create the String content of a feature file from a sequence of FeatureBundles
 
 def featureDependencies(features: Map[String, Seq[FeatureBundle]]): Seq[Dependency] = {
-  features.values.flatten.map(_.dependency.copy(exclusions = Seq("*" % "*"))).toList
+  features.values.flatten.map(_.dependency)
+    .foldLeft(List[Dependency]()) { (ds, n) =>
+      if (ds.exists(d =>
+        d.gav.groupId == n.gav.groupId &&
+          d.gav.artifactId == n.gav.artifactId &&
+          d.gav.version == n.gav.version &&
+          d.classifier == n.classifier &&
+          d.scope == n.scope
+      )) ds
+      else n :: ds
+    }
 }
 
 // This is the content of the feature file
