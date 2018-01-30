1da09158b5b24d787468851cff865b8d46be726c
Andreas Gies <andreas@wayofquality.de>
2018-01-30 13:05:35 +0100

    After releasing 2.4.0-RC1

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.4.0-RC1"
+  val blendedVersion = "2.4.0-RC2-SNAPSHOT"
 
   val javaVersion = "1.8"
 
