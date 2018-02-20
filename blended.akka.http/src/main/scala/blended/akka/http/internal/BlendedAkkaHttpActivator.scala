cad77db5d3486c9696c2cf562e3b03f6460677e8
Tobias Roeser <le.petit.fou@web.de>
2018-02-20 13:41:40 +0100

    User shared helper class to handle default values in config

--- blended.akka.http/src/main/scala/blended/akka/http/internal/BlendedAkkaHttpActivator.scala
+++ blended.akka.http/src/main/scala/blended/akka/http/internal/BlendedAkkaHttpActivator.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import blended.akka.ActorSystemWatching
 import domino.DominoActivator
+import blended.util.config.Implicits._
 
 class BlendedAkkaHttpActivator extends DominoActivator with ActorSystemWatching {
 
@@ -14,20 +15,13 @@ class BlendedAkkaHttpActivator extends DominoActivator with ActorSystemWatching 
     // reuse the blended akka system
     whenActorSystemAvailable { cfg =>
 
-      // Read the bundle config value for the given `key` or return the default, when the key is not present
-      def getOrDefault[T](key: String, default: T): T = if (cfg.config.hasPath(key)) {
-        default match {
-          case x: String => cfg.config.getString(key).asInstanceOf[T]
-          case x: Int => cfg.config.getInt(key).asInstanceOf[T]
-          case x: Long => cfg.config.getLong(key).asInstanceOf[T]
-        }
-      } else default
+      val config = cfg.config
+      
+      val httpHost = config.getString("host", "0.0.0.0")
+      val httpPort = config.getInt("port", 8080)
 
-      val httpHost = getOrDefault("host", "0.0.0.0")
-      val httpPort = getOrDefault("port", 8080)
-
-      //      val httpsHost = getOrDefault("ssl.host", "0.0.0.0")
-      //      val httpsPort = getOrDefault("ssl.port", 8443)
+      //      val httpsHost = config.getString("ssl.host", "0.0.0.0")
+      //      val httpsPort = config.getInt("ssl.port", 8443)
 
       implicit val actorSysten = cfg.system
       implicit val actorMaterializer = ActorMaterializer()
