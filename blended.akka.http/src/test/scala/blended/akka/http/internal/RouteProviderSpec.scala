0cec6d83e70f468055c4b8bb3b1c20d8a823ff7c
Tobias Roeser <le.petit.fou@web.de>
2018-02-20 09:09:36 +0100

    Refined tests

--- blended.akka.http/src/test/scala/blended/akka/http/internal/RouteProviderSpec.scala
+++ blended.akka.http/src/test/scala/blended/akka/http/internal/RouteProviderSpec.scala
@@ -1,17 +1,13 @@
 package blended.akka.http.internal
 
+import scala.util.Try
+
 import org.scalatest.FreeSpec
+
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import domino.capsule.DynamicCapsuleContext
-import domino.DominoActivator
-import com.sun.net.httpserver.HttpServer
 import blended.akka.http.HttpContext
 import blended.akka.http.SimpleHttpContext
-import scala.util.Try
-import scala.concurrent.Promise
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-import java.util.concurrent.TimeUnit
+import domino.DominoActivator
 
 class RouteProviderSpec
     extends FreeSpec
@@ -40,16 +36,16 @@ class RouteProviderSpec
 
   "Inside a dynamic (OSGi) environment, the RouteProvider should" - {
 
-    "handle new HttpContext registrations" in {
+    "handle comming and going HttpContext registrations" in {
       withPojoServiceRegistry { sr =>
 
-        val activator1 = new DominoActivator() {
+        val serviceBundle = new DominoActivator() {
           whenBundleActive {
             routeProvider.dynamicAdapt(capsuleContext, bundleContext)
           }
         }
 
-        val activator2 = new DominoActivator() {
+        val routeBundle = new DominoActivator() {
           whenBundleActive {
             import akka.http.scaladsl.server.Directives._
             val route = pathEnd {
@@ -61,50 +57,34 @@ class RouteProviderSpec
           }
         }
 
-        try {
-          activator1.start(sr.getBundleContext())
+        val bundleContext = sr.getBundleContext()
 
-          //          {
-          //            val p1 = Promise[String]()
+        try {
+
+          serviceBundle.start(bundleContext)
 
           // precondition
           Get("/demo") ~> route ~> check {
             assert(handled === false)
-            //              p1.success("1")
           }
 
-          //            Await.result(p1.future, Duration(1, TimeUnit.SECONDS))
-          //          }
-
-          activator2.start(sr.getBundleContext())
-
-          //          {
-          //            val p2 = Promise[String]()
+          routeBundle.start(bundleContext)
 
           Get("/demo") ~> route ~> check {
-            //              p2.success("1")
             assert(responseAs[String] === "HELLO")
           }
 
-          //            Await.result(p2.future, Duration(1, TimeUnit.SECONDS))
-          //          }
+          routeBundle.stop(bundleContext)
 
-          activator2.stop(sr.getBundleContext())
-
-          //          {
-          //            val p3 = Promise[String]()
           // postcondition
           Get("/demo") ~> route ~> check {
-            //              p3.success("1")
             assert(handled === false)
           }
 
-          //            Await.result(p3.future, Duration(1, TimeUnit.SECONDS))
-          //          }
-
         } finally {
-          Try { activator1.stop(sr.getBundleContext) }
-          Try { activator2.stop(sr.getBundleContext) }
+          // cleanup
+          Try { serviceBundle.stop(bundleContext) }
+          Try { routeBundle.stop(bundleContext) }
         }
       }
     }
