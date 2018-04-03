e0d0e0c79b7c038762d7b865a3a2b4ca0edbc7c0
Andreas Gies <andreas@wayofquality.de>
2018-04-03 19:46:26 +0200

    After releasing 2.4.0-RC6

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.4.0-RC6"
+  val blendedVersion = "2.4.0-RC7-SNAPSHOT"
 
   val javaVersion = "1.8"
 
