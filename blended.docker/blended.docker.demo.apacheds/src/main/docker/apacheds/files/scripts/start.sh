78c8d44b153bd35945fc06f153e3328aa8fe4314
Andreas Gies <andreas@wayofquality.de>
2018-04-11 21:10:06 +0200

    Adding some object to LDAP initial objects

--- blended.docker/blended.docker.demo.apacheds/src/main/docker/apacheds/files/scripts/start.sh
+++ blended.docker/blended.docker.demo.apacheds/src/main/docker/apacheds/files/scripts/start.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-export ADMIN_PWD=blended
+set -e
+set -x
+
 export DOMAIN_NAME=blended
+export SYSTEM_PWD=blended
 
 APACHE_DS_VERSION=2.0.0_M24
 
-function stopADS {
+function shaPassword() {
+  export HASHED_PWD=`echo $1 | sha1sum - | awk '{print "{SSHA}"$1}'`
+}
+
+function stopADS() {
   /etc/init.d/apacheds-${APACHE_DS_VERSION}-default stop
 }
 
-function startADS {
+function startADS() {
 
   if [[ -n "$1" ]]; then
     START_MODE=$1
@@ -24,12 +31,12 @@ function startADS {
   fi
 }
 
-function restartADS {
+function restartADS() {
   stopADS
   startADS $*
 }
 
-function loadLdif {
+function loadLdif() {
   envsubst < /opt/apacheds/ldif/$2.ldif > /tmp/$2.ldif
   ldapmodify -c -a -f /tmp/$2.ldif -h localhost -p 10389 -D "uid=admin,ou=system" -w $1
 }
@@ -38,22 +45,23 @@ function loadLdif {
 startADS start 5
 
 # then we change the admin password
+# shaPassword $SYSTEM_PWD
+export HASHED_PWD=$SYSTEM_PWD
 loadLdif secret admin_pwd
 
 # Restart to apply changes
 restartADS start 5
 
 # create a new partition
-envsubst < /opt/apacheds/ldif/top_domain.ldif > /tmp/top_domain.ldif
-export TOP_CTXT=$(base64 -w 0 /tmp/top_domain.ldif)
-loadLdif $ADMIN_PWD partition
-ldapdelete "ads-partitionId=example,ou=partitions,ads-directoryServiceId=default,ou=config" -r -p 10389 -h localhost -D "uid=admin,ou=system" -w $ADMIN_PWD
-ldapdelete "dc=example,dc=com" -p 10389 -h localhost -D "uid=admin,ou=system" -r -w $ADMIN_PWD
+loadLdif $SYSTEM_PWD partition
+ldapdelete "ads-partitionId=example,ou=partitions,ads-directoryServiceId=default,ou=config" -r -p 10389 -h localhost -D "uid=admin,ou=system" -w $SYSTEM_PWD
+ldapdelete "dc=example,dc=com" -p 10389 -h localhost -D "uid=admin,ou=system" -r -w $SYSTEM_PWD
 
 restartADS start 5
 
 # create the top level entries
-loadLdif $ADMIN_PWD top_domain
-loadLdif $ADMIN_PWD top_objects
+loadLdif $SYSTEM_PWD top_domain
+loadLdif $SYSTEM_PWD top_objects
 
 restartADS console
+
