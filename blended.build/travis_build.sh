ed03d3c462b46be6d7daaaeafb1d8872a3228938
Andreas Gies <andreas@wayofquality.de>
2018-05-09 13:28:45 +0200

    Adding simple travis build script without integration tests

---
+++ blended.build/travis_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e 
+
+SCRIPT_DIR=$(dirname $0)
+BUILD_DIR=$SCRIPT_DIR/..
+
+THIRD_PARTY_DIR=$BUILD_DIR/target/3rdparty
+
+rm -Rf $THIRD_PARTY_DIR
+mkdir -p $THIRD_PARTY_DIR
+
+cd $THIRD_PARTY_DIR
+THIRD_PARTY_DIR=$(pwd)
+
+git clone ssh://git@github.com/woq-blended/react4s.git 
+git clone ssh://git@github.com/woq-blended/router4s.git
+
+cd $THIRD_PARTY_DIR/react4s
+sbt publishLocal
+
+cd $THIRD_PARTY_DIR/router4s
+sbt publishLocal
+
+mvn clean install -P build 
+
