bd9ce670a4cecd9d3288c078e6309f0447ebf5ff
Andreas Gies <andreas@wayofquality.de>
2018-05-24 16:14:07 +0200

    Bumping version number

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.4.0"
+  val blendedVersion = "2.5.0-M1-SNAPSHOT"
 
   val javaVersion = "1.8"
 
