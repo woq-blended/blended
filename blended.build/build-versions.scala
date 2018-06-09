a9f8a73adc237838d0af78a050cb753230f48a4c
Andreas Gies <andreas@wayofquality.de>
2018-06-09 16:25:08 +0200

    Releasing 2.5.0-M3

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.5.0-M3-SNAPSHOT"
+  val blendedVersion = "2.5.0-M3"
 
   val javaVersion = "1.8"
 
