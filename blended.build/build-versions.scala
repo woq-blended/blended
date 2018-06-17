90e83e115088cfe1da7adac6ca5e8e7be118ef13
Andreas Gies <andreas@wayofquality.de>
2018-06-17 22:54:54 +0200

    Releasing 2.5.0-M4

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.5.0-M4-SNAPSHOT"
+  val blendedVersion = "2.5.0-M4"
 
   val javaVersion = "1.8"
 
