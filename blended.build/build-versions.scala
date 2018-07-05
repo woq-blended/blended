160a03d1fca3605e640cf1354e445ee0c8a6bb04
Andreas Gies <andreas@wayofquality.de>
2018-07-05 13:05:00 +0200

    After releasing 2.5.0-M6

--- blended.build/build-versions.scala
+++ blended.build/build-versions.scala
@@ -1,7 +1,7 @@
 object BlendedVersions {
 
   val blendedGroupId = "de.wayofquality.blended"
-  val blendedVersion = "2.5.0-M6"
+  val blendedVersion = "2.5.0-M7-SNAPSHOT"
 
   val javaVersion = "1.8"
 
