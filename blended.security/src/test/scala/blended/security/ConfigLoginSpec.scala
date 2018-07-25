ca3ebf1920956ad7c56c4a4b1b183cb39a69c0e8
Andreas Gies <andreas@wayofquality.de>
2018-07-25 19:42:35 +0200

    Adding spec for Simple Login Module

---
+++ blended.security/src/test/scala/blended/security/ConfigLoginSpec.scala
@@ -0,0 +1,47 @@
+package blended.security
+
+import java.io.File
+
+import blended.security.internal.SecurityActivator
+import blended.testsupport.BlendedTestSupport
+import blended.testsupport.pojosr.{PojoSrTestHelper, SimplePojosrBlendedContainer}
+import javax.security.auth.login.LoginContext
+import org.scalatest.{DoNotDiscover, FreeSpec}
+
+class ConfigLoginSpec extends FreeSpec
+  with SimplePojosrBlendedContainer
+  with PojoSrTestHelper {
+
+  private[this] val user = "andreas"
+  private[this] val pwd = "mysecret"
+
+  private[this] val log = org.log4s.getLogger
+
+  "the security activator should" - {
+
+    "initialise the Login Module correctly" in {
+
+      val baseDir = new File(BlendedTestSupport.projectTestOutput, "simple").getAbsolutePath()
+
+      withSimpleBlendedContainer(baseDir) { sr =>
+
+        withStartedBundle(sr)(symbolicName = "blended.security", activator = Some(() => new SecurityActivator())) { sr =>
+
+          val lc : LoginContext = new LoginContext("Test", new PasswordCallbackHandler(user, pwd.toCharArray()))
+          lc.login()
+
+          val sub = lc.getSubject()
+
+          val ref = sr.getServiceReference(classOf[BlendedPermissionManager].getName())
+          val mgr = sr.getService(ref).asInstanceOf[BlendedPermissionManager]
+
+          val groups = mgr.permissions(sub)
+
+          assert(groups.size == 2)
+          assert(groups.contains("admins"))
+          assert(groups.contains("blended"))
+        }
+      }
+    }
+  }
+}
