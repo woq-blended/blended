b5d066bff109ba8efe64afcaff6aa0dd2dc1a94e
Tobias Roeser <le.petit.fou@web.de>
2018-08-09 11:25:09 +0200

    Removed internal Logger class and use blended.util.logger instead

--- blended.launcher/src/runner/resources/bin/blended.sh
+++ blended.launcher/src/runner/resources/bin/blended.sh
@@ -80,6 +80,7 @@ ${BLENDED_HOME}/lib/blended.launcher-@blended.launcher.version@.jar;\
 ${BLENDED_HOME}/lib/config-@typesafe.config.version@.jar;\
 ${BLENDED_HOME}/lib/org.osgi.core-@org.osgi.core.version@.jar;\
 ${BLENDED_HOME}/lib/blended.updater.config-@blended.updater.config.version@.jar;\
+${BLENDED_HOME}/lib/blended.util.logging-@blended.util.logging.version@.jar;\
 ${BLENDED_HOME}/lib/de.tototec.cmdoption-@cmdoption.version@.jar;\
 ${BLENDED_HOME}/lib/scala-library-@scala.library.version@.jar;\
 ${BLENDED_HOME}/lib/slf4j-api-@slf4j.version@.jar;\
