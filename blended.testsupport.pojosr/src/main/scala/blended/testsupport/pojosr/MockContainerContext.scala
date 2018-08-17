9fbb1fa06c8cd287bfe12bb1f7c9335139a6162d
Tobias Roeser <le.petit.fou@web.de>
2018-08-17 15:55:33 +0200

    Fixed too-eary initialization of system properties config resolver

--- blended.testsupport.pojosr/src/main/scala/blended/testsupport/pojosr/MockContainerContext.scala
+++ blended.testsupport.pojosr/src/main/scala/blended/testsupport/pojosr/MockContainerContext.scala
@@ -1,9 +1,12 @@
 package blended.testsupport.pojosr
 
 import java.io.File
+import java.util.Properties
 
 import blended.container.context.api.ContainerContext
-import com.typesafe.config.{Config, ConfigFactory, ConfigParseOptions}
+import com.typesafe.config.{ Config, ConfigFactory, ConfigParseOptions }
+import com.typesafe.config.ConfigObject
+import com.typesafe.config.impl.Parseable
 
 class MockContainerContext(baseDir: String) extends ContainerContext {
 
@@ -19,13 +22,31 @@ class MockContainerContext(baseDir: String) extends ContainerContext {
 
   override def getContainerHostname(): String = "localhost"
 
+  private def getSystemProperties(): Properties = {
+    // Avoid ConcurrentModificationException due to parallel setting of system properties by copying properties
+    val systemProperties = System.getProperties()
+    val systemPropertiesCopy = new Properties()
+    systemPropertiesCopy.putAll(systemProperties)
+    systemPropertiesCopy
+  }
+
+  private def loadSystemProperties(): ConfigObject = {
+    Parseable.newProperties(
+      getSystemProperties(),
+      ConfigParseOptions.defaults().setOriginDescription("system properties")
+    ).parse()
+  }
+
   override def getContainerConfig(): Config = {
-    val sysProps = ConfigFactory.systemProperties()
+    val sysProps = loadSystemProperties()
     val envProps = ConfigFactory.systemEnvironment()
 
     ConfigFactory.parseFile(
       new File(getProfileConfigDirectory(), "application.conf"),
       ConfigParseOptions.defaults().setAllowMissing(false)
-    ).withFallback(sysProps).withFallback(envProps).resolve()
+    ).
+      withFallback(sysProps).
+      withFallback(envProps).
+      resolve()
   }
 }
