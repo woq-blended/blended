f7be1efd5183a0963dc4804a63205c75fb906e48
Andreas Gies <andreas@wayofquality.de>
2018-08-18 12:12:56 +0200

    Reviewing OSGi settings for login service

--- blended.security.login.rest/src/main/scala/blended/security/login/rest/internal/RestLoginActivator.scala
+++ blended.security.login.rest/src/main/scala/blended/security/login/rest/internal/RestLoginActivator.scala
@@ -4,13 +4,16 @@ import blended.akka.ActorSystemWatching
 import blended.akka.http.{HttpContext, SimpleHttpContext}
 import blended.security.BlendedPermissionManager
 import blended.security.login.api.TokenStore
+import blended.util.logging.Logger
 import domino.DominoActivator
 
 class RestLoginActivator extends DominoActivator with ActorSystemWatching {
 
+  private[this] val log = Logger[RestLoginActivator]
   whenBundleActive {
     whenActorSystemAvailable { osgiCfg =>
       whenServicesPresent[TokenStore, BlendedPermissionManager] { (store, mgr) =>
+        log.info("Starting login REST service")
         val svc = new LoginService(store, mgr)(osgiCfg.system.dispatcher)
         SimpleHttpContext("login", svc.route).providesService[HttpContext]
       }
