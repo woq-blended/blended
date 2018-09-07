85b571f291d287d156b2ac900248702ab016dd79
Tobias Roeser <le.petit.fou@web.de>
2018-09-07 09:14:28 +0200

    import cleanup

--- project/BlendedUtilLogging.scala
+++ project/BlendedUtilLogging.scala
@@ -1,11 +1,9 @@
-import sbt._
-
 object BlendedUtilLogging extends ProjectSettings(
   "blended.util.logging",
   "Logging utility classes to use in other bundles."
 ) {
 
-  override val libDependencies : Seq[ModuleID] = Seq(
+  override val libDependencies : Seq[sbt.ModuleID] = Seq(
     Dependencies.slf4j
   )
 }
