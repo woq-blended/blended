65352435838ca96b8722c5f1036d47a62347ae02
Tobias Roeser <le.petit.fou@web.de>
2018-09-21 10:52:51 +0200

    Added a LoggingFreeSpecLike

--- blended.testsupport/src/main/scala/blended/testsupport/scalatest/LoggingFreeSpec.scala
+++ blended.testsupport/src/main/scala/blended/testsupport/scalatest/LoggingFreeSpec.scala
@@ -3,9 +3,16 @@ package blended.testsupport.scalatest
 import blended.util.logging.Logger
 import org.scalatest.Args
 import org.scalatest.FreeSpec
+import org.scalatest.FreeSpecLike
 import org.scalatest.Status
+import org.scalatest.Suite
 
-trait LoggingFreeSpec extends FreeSpec {
+/**
+  * Same as [[org.scalatest.FreeSpecLike]] but log the start and the end of each test case to SLF4j in Debug level.
+  *
+  * @see LoggingFreeSpec
+  */
+trait LoggingFreeSpecLike extends FreeSpecLike {
 
   abstract protected override def runTest(testName: String, args: Args): Status = {
     val log: Logger = Logger[this.type]
@@ -18,3 +25,10 @@ trait LoggingFreeSpec extends FreeSpec {
   }
 
 }
+
+/**
+  * Same as [[org.scalatest.FreeSpec]] but log the start and the end of each test case to SLF4j in Debug level.
+  *
+  * @see LoggingFreeSpecLike
+  */
+class LoggingFreeSpec extends FreeSpec with LoggingFreeSpecLike
