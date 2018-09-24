d2e2ad805b3244c689a7e94dc22c48f6f6f20ca4
Andreas Gies <andreas@wayofquality.de>
2018-09-24 15:46:27 +0200

    Fine tuning build and release scripts. Bumping version number

--- verifyPublish.sh
+++ verifyPublish.sh
@@ -16,15 +16,20 @@ fi
 # same name as the version to be released. If so, we will set 
 # the publishCmd to ciRelease, otherwise we will break the build.
 
-if [[ ISSNAPSHOT -eq 1 ]]; then 
-  export PUBCMD="ciPublish"
+if [[ ISSNAPSHOT -eq 1 ]]; then
+  if [[ $TRAVIS_TAG != "" ]]; then
+    echo "Found a tagged version with SNAPSHOT Qualifier, falling back to build without publish"
+    export PUBCMD="ciBuild"
+  else
+    export PUBCMD="ciPublish"
+  fi
 else 
   if [[ $TRAVIS_TAG = $VERSION ]]; then 
     export PUBCMD="ciRelease"
   else 
-    export PUBCMD=
     echo "A tag matching the version to be released ($VERSION) is required to push the release to Maven central."
-    exit 1
+    echo "Falling back to a normal build without publishing."
+    export PUBCMD="ciBuild"
   fi
 fi
 
