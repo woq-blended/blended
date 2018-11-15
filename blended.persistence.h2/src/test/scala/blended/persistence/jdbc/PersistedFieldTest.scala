080c79188cefdf0f914504d74e5eaacc87dfd875
Tobias Roeser <le.petit.fou@web.de>
2018-11-15 08:30:18 +0100

    added test case

--- blended.persistence.h2/src/test/scala/blended/persistence/jdbc/PersistedFieldTest.scala
+++ blended.persistence.h2/src/test/scala/blended/persistence/jdbc/PersistedFieldTest.scala
@@ -1,7 +1,8 @@
 package blended.persistence.jdbc
 
-import org.scalatest.FreeSpec
 import scala.collection.JavaConverters._
+
+import org.scalatest.FreeSpec
 
 class PersistedFieldTest extends FreeSpec {
 
@@ -10,26 +11,22 @@ class PersistedFieldTest extends FreeSpec {
       Map("key" -> null).asJava,
       Seq(
         PersistedField(fieldId = 1, name = "key", typeName = TypeName.Null)
-      )
-    ),
+      )),
     ("String",
       Map("key" -> "value").asJava,
       Seq(
         PersistedField(fieldId = 1, name = "key", valueString = Some("value"), typeName = TypeName.String)
-      )
-    ),
+      )),
     ("Boolean",
       Map("key" -> true).asJava,
       Seq(
         PersistedField(fieldId = 1, name = "key", valueLong = Some(1), typeName = TypeName.Boolean)
-      )
-    ),
+      )),
     ("Long",
       Map("key" -> Long.MaxValue).asJava,
       Seq(
         PersistedField(fieldId = 1, name = "key", valueLong = Some(Long.MaxValue), typeName = TypeName.Long)
-      )
-    ),
+      )),
     (
       "Byte",
       Map("key" -> Byte.MaxValue).asJava,
@@ -47,14 +44,12 @@ class PersistedFieldTest extends FreeSpec {
       Map("key" -> Double.MaxValue).asJava,
       Seq(
         PersistedField(fieldId = 1, name = "key", valueDouble = Some(Double.MaxValue), typeName = TypeName.Double)
-      )
-    ),
+      )),
     ("Float",
       Map("key" -> Float.MaxValue).asJava,
       Seq(
         PersistedField(fieldId = 1, name = "key", valueDouble = Some(Float.MaxValue), typeName = TypeName.Float)
-      )
-    ),
+      )),
     ("List[String]",
       Map("key" -> List("v1", "v2", "v3").asJava).asJava,
       Seq(
@@ -62,8 +57,7 @@ class PersistedFieldTest extends FreeSpec {
         PersistedField(fieldId = 2, baseFieldId = Some(1), name = "0", valueString = Some("v1"), typeName = TypeName.String),
         PersistedField(fieldId = 3, baseFieldId = Some(1), name = "1", valueString = Some("v2"), typeName = TypeName.String),
         PersistedField(fieldId = 4, baseFieldId = Some(1), name = "2", valueString = Some("v3"), typeName = TypeName.String)
-      )
-    ),
+      )),
     ("List[Map]",
       Map("key" -> List(Map("ik" -> "iv1", "ik2" -> 2).asJava, Map("second" -> "value").asJava).asJava).asJava,
       Seq(
@@ -73,8 +67,7 @@ class PersistedFieldTest extends FreeSpec {
         PersistedField(fieldId = 4, baseFieldId = Some(2), name = "ik2", valueLong = Some(2L), typeName = TypeName.Int),
         PersistedField(fieldId = 5, baseFieldId = Some(1), name = "1", typeName = TypeName.Object),
         PersistedField(fieldId = 6, baseFieldId = Some(5), name = "second", valueString = Some("value"), typeName = TypeName.String)
-      )
-    ),
+      )),
     ("Config object",
       Map(
         "key1" -> "value1",
@@ -100,8 +93,7 @@ class PersistedFieldTest extends FreeSpec {
           PersistedField(fieldId = 7, name = "k3b", baseFieldId = Some(5), typeName = TypeName.Array),
           PersistedField(fieldId = 8, name = "0", baseFieldId = Some(7), typeName = TypeName.String, valueString = Some("v3b1")),
           PersistedField(fieldId = 9, name = "1", baseFieldId = Some(7), typeName = TypeName.String, valueString = Some("v3b2"))
-        )
-    ),
+        )),
     (
       "LongString",
       Map("key" -> List.fill(500)("x").mkString).asJava,
@@ -124,6 +116,44 @@ class PersistedFieldTest extends FreeSpec {
     s"PersistedField.toJuMap with a ${data._1} entry" in {
       assert(PersistedField.toJuMap(data._3) === data._2)
     }
+  }
+
+  "Complex Map converts to PersistedField and back" in {
+    val example1 = Map(
+      "containerId" -> "1",
+      "outstandingActions" -> List(
+        Map(
+        "kind" -> "AddRuntimeConfig",
+        "runtimeConfig" -> Map(
+          "resolvedFeatures" -> List().asJava,
+          "startLevel" -> 10,
+          "name" -> "rc",
+          "systemProperties" -> Map().asJava,
+          "bundles" -> List().asJava,
+          "frameworkProperties" -> Map().asJava,
+          "features" -> List().asJava,
+          "defaultStartLevel" -> 10,
+          "version" -> "1",
+          "properties" -> Map().asJava,
+          "resources" -> List().asJava
+        ).asJava
+      ).asJava,
+        Map(
+        "kind" -> "AddOverlayConfig",
+        "overlayConfig" -> Map(
+          "name" -> "oc",
+          "version" -> "1",
+          "generatedConfigs" -> List().asJava,
+          "properties" -> Map().asJava
+        ).asJava
+      ).asJava
+      ).asJava,
+      "profiles" -> List().asJava,
+      "syncTimeStamp" -> null
+    ).asJava
+
+    assert(PersistedField.toJuMap(PersistedField.extractFieldsWithoutDataId(example1)) === example1)
+
   }
 
 }
