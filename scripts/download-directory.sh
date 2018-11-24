e71aa4f3f85a584853f80edbf5de98d591e40762
Andreas Gies <andreas@wayofquality.de>
2018-11-24 13:52:54 +0100

    Fine tuning .travis.yml for integration tests

--- scripts/download-directory.sh
+++ scripts/download-directory.sh
@@ -8,7 +8,7 @@ set DOWNLOAD_FILE=$2
 mkdir -p $DOWNLOAD_DIR
 cd $DOWNLOAD_DIR
 
-$TRAVIS_BUILD_DIR/scripts/dropbox_uploader.sh download travis/$1/$DOWNLOAD_FILE.tgz $DOWNLOAD_FILE.tgz
+$TRAVIS_BUILD_DIR/scripts/dropbox_uploader.sh download travis/$TRAVIS_BUILD_NUMBER/$DOWNLOAD_FILE.tgz $DOWNLOAD_FILE.tgz
 tar -xzf $DOWNLOAD_FILE.tgz
 rm $DOWNLOAD_FILE.tgz
 
