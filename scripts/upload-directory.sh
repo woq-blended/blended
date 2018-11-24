433b89bbadf45bb272d8d2aad03de131d6d7d9ae
Andreas Gies <andreas@wayofquality.de>
2018-11-24 12:30:10 +0100

    Fine tuning .travis.yml for integration tests

--- scripts/upload-directory.sh
+++ scripts/upload-directory.sh
@@ -8,4 +8,4 @@ UPLOAD_FILE=$2
 cd $UPLOAD_DIR
 tar -czf ../$UPLOAD_FILE.tgz .
 
-$TRAVIS_BUILD_DIR/scripts/dropbox_uploader.sh upload $UPLOAD_FILE.tgz travis/$1/$UPLOAD_FILE.tgz
+$TRAVIS_BUILD_DIR/scripts/dropbox_uploader.sh upload ../$UPLOAD_FILE.tgz travis/$TRAVIS_BUILD_NUMBER/$UPLOAD_FILE.tgz
