281f17bca38239652c20e0035df6ddb7c38c6a56
Andreas Gies <andreas@wayofquality.de>
2018-11-24 18:45:20 +0100

    Fine tuning .travis.yml for integration tests

--- scripts/upload-results.sh
+++ scripts/upload-results.sh
@@ -6,4 +6,4 @@ rm -Rf $TRAVIS_BUILD_DIR/target/results
 mkdir -p $TRAVIS_BUILD_DIR/target/results
 find $TRAVIS_BUILD_DIR -name "TEST*.xml" -printf "cp %p $TRAVIS_BUILD_DIR/target/results/%f\n" | sh
 
-$TRAVIS_BUILD_DIR/scripts/upload-directory.sh $TRAVIS_BUILD_DIR/target/results results
+$TRAVIS_BUILD_DIR/scripts/upload-directory.sh $TRAVIS_BUILD_DIR/target target
