ae45260e4b733d1ff6b6988c8621f74365970c0d
Tobias Roeser <le.petit.fou@web.de>
2018-11-27 10:57:32 +0100

    Improved naming of test cases

--- blended.mgmt.rest/src/test/scala/blended/mgmt/rest/internal/CollectorServicePojosrSpec.scala
+++ blended.mgmt.rest/src/test/scala/blended/mgmt/rest/internal/CollectorServicePojosrSpec.scala
@@ -33,7 +33,6 @@ class CollectorServicePojosrSpec extends SimplePojoContainerSpec
 
   override def baseDir: String = new File(BlendedTestSupport.projectTestOutput, "container").getAbsolutePath()
 
-
   override def bundles: Seq[(String, BundleActivator)] = Seq(
     "blended.akka" -> new BlendedAkkaActivator(),
     "blended.akka.http" -> new BlendedAkkaHttpActivator(),
@@ -74,40 +73,42 @@ class CollectorServicePojosrSpec extends SimplePojoContainerSpec
 
   val versionUrl = uri"${serverUrl}/version"
 
-  s"GET ${versionUrl} should return the version" in {
-    withServer { sr =>
-      val response = sttp.sttp.get(versionUrl).send()
-      assert(response.body === Right("\"0.0.0\""))
-    }
-  }
+  s"REST-API with a self-hosted HTTP server at ${serverUrl}" - {
 
-  "OverlayConfig" - {
-
-    val url = uri"${serverUrl}/overlayConfig"
-
-    "GET without credentials should fail with 401" in logException {
-      // we currently do not require any permission for GET
-      pending
-      withServer { server =>
-        val response = sttp.sttp.get(url).send()
-        assert(response.code === 401)
+    s"GET ${versionUrl} should return the version" in {
+      withServer { sr =>
+        val response = sttp.sttp.get(versionUrl).send()
+        assert(response.body === Right("\"0.0.0\""))
       }
     }
 
-    "initial GET should return empty overlay list" in logException {
-      withServer { server =>
-        val response = sttp.sttp.get(url)
-          .auth.basic("tester", "mysecret")
-          .send()
-        assert(response.code === 200)
-        val ocs = Unpickle[Seq[OverlayConfig]].fromString(response.unsafeBody).get
-        assert(ocs.size === 0)
-      }
-    }
+    "OverlayConfig" - {
 
-    "POST allows upload of new OverlayConfig" in logException {
-      val o1 =
-        """name = "jvm-medium"
+      val url = uri"${serverUrl}/overlayConfig"
+
+      "GET without credentials should fail with 401 - pending until feature is enabled" in logException {
+        // we currently do not require any permission for GET
+        pending
+        withServer { server =>
+          val response = sttp.sttp.get(url).send()
+          assert(response.code === 401)
+        }
+      }
+
+      "initial GET should return empty overlay list" in logException {
+        withServer { server =>
+          val response = sttp.sttp.get(url)
+            .auth.basic("tester", "mysecret")
+            .send()
+          assert(response.code === 200)
+          val ocs = Unpickle[Seq[OverlayConfig]].fromString(response.unsafeBody).get
+          assert(ocs.size === 0)
+        }
+      }
+
+      "POST allows upload of new OverlayConfig" in logException {
+        val o1 =
+          """name = "jvm-medium"
           |version = "1"
           |properties = {
           |  "blended.launcher.jvm.xms" = "768M"
@@ -116,108 +117,109 @@ class CollectorServicePojosrSpec extends SimplePojoContainerSpec
           |}
           |""".stripMargin
 
-      val oc = OverlayConfigCompanion.read(ConfigFactory.parseString(o1)).get
+        val oc = OverlayConfigCompanion.read(ConfigFactory.parseString(o1)).get
 
-      withServer { server =>
-        val responsePost = sttp.sttp
-          .post(url)
-          .body(Pickle.intoString(oc))
-          .header(sttp.HeaderNames.ContentType, sttp.MediaTypes.Json)
-          .auth.basic("tester", "mysecret")
-          .send()
-        assert(responsePost.code === 200)
-        assert(responsePost.unsafeBody === "\"Registered jvm-medium-1\"")
+        withServer { server =>
+          val responsePost = sttp.sttp
+            .post(url)
+            .body(Pickle.intoString(oc))
+            .header(sttp.HeaderNames.ContentType, sttp.MediaTypes.Json)
+            .auth.basic("tester", "mysecret")
+            .send()
+          assert(responsePost.code === 200)
+          assert(responsePost.unsafeBody === "\"Registered jvm-medium-1\"")
 
-        val responseGet = sttp.sttp.get(url)
-          .auth.basic("tester", "mysecret")
-          .send()
-        assert(responseGet.code === 200)
-        val ocs = Unpickle[Seq[OverlayConfig]].fromString(responseGet.unsafeBody).get
-        assert(ocs.size === 1)
-        assert(ocs.find(_.name == "jvm-medium").isDefined)
+          val responseGet = sttp.sttp.get(url)
+            .auth.basic("tester", "mysecret")
+            .send()
+          assert(responseGet.code === 200)
+          val ocs = Unpickle[Seq[OverlayConfig]].fromString(responseGet.unsafeBody).get
+          assert(ocs.size === 1)
+          assert(ocs.find(_.name == "jvm-medium").isDefined)
+        }
       }
+
     }
 
-  }
+    "Upload deployment pack" - {
 
-  "Upload deployment pack" - {
+      val uploadUrl = uri"${serverUrl}/profile/upload/deploymentpack/artifacts"
+      val emptyPackFile = new File(BlendedTestSupport.projectTestOutput, "test.pack.empty-1.0.0.zip")
+      val packFile = new File(BlendedTestSupport.projectTestOutput, "test.pack.minimal-1.0.0.zip")
 
-    val uploadUrl = uri"${serverUrl}/profile/upload/deploymentpack/artifacts"
-    val emptyPackFile = new File(BlendedTestSupport.projectTestOutput, "test.pack.empty-1.0.0.zip")
-    val packFile = new File(BlendedTestSupport.projectTestOutput, "test.pack.minimal-1.0.0.zip")
+      s"Uploading with missing credentials should fail with 401" in logException {
+        withServer { server =>
+          assert(packFile.exists())
 
-    s"Uploading with missing credentials should fail with 401" in logException {
-      withServer { server =>
-        assert(packFile.exists())
-
-        val response = sttp.sttp.
-          multipartBody(sttp.multipartFile("file", emptyPackFile)).
-          post(uploadUrl).
-          send()
-        assert(response.code === 401)
+          val response = sttp.sttp.
+            multipartBody(sttp.multipartFile("file", emptyPackFile)).
+            post(uploadUrl).
+            send()
+          assert(response.code === 401)
+        }
       }
-    }
 
-    s"Uploading with wrong credentials should fail with 401" in logException {
-      withServer { server =>
-        assert(packFile.exists())
+      s"Uploading with wrong credentials should fail with 401" in logException {
+        withServer { server =>
+          assert(packFile.exists())
 
-        val response = sttp.sttp.
-          multipartBody(sttp.multipartFile("file", emptyPackFile)).
-          auth.basic("unknown", "pass").
-          post(uploadUrl).
-          send()
-        assert(response.code === 401)
+          val response = sttp.sttp.
+            multipartBody(sttp.multipartFile("file", emptyPackFile)).
+            auth.basic("unknown", "pass").
+            post(uploadUrl).
+            send()
+          assert(response.code === 401)
+        }
       }
-    }
 
-    s"Multipart POST with empty profile (no bundles) should fail with validation errors" in logException {
-      withServer { server =>
-        assert(emptyPackFile.exists() === true)
+      s"Multipart POST with empty profile (no bundles) should fail with validation errors" in logException {
+        withServer { server =>
+          assert(emptyPackFile.exists() === true)
 
-        val response = sttp.sttp.
-          multipartBody(sttp.multipartFile("file", emptyPackFile)).
-          auth.basic("tester", "mysecret").
-          post(uploadUrl).
-          send()
+          val response = sttp.sttp.
+            multipartBody(sttp.multipartFile("file", emptyPackFile)).
+            auth.basic("tester", "mysecret").
+            post(uploadUrl).
+            send()
 
-        log.info("body: " + response.body)
-        log.info("headers: " + response.headers)
-        log.info("response: " + response)
+          log.info("body: " + response.body)
+          log.info("headers: " + response.headers)
+          log.info("response: " + response)
 
-        assert(response.code === 422)
-        assert(response.statusText === "Unprocessable Entity")
-        assert(response.body.isLeft)
-        assert(response.body.left.get ===
-          "Could not process the uploaded deployment pack file. Reason: requirement failed: " +
-          "A ResolvedRuntimeConfig needs exactly one bundle with startLevel '0', but this one has (distinct): 0")
+          assert(response.code === 422)
+          assert(response.statusText === "Unprocessable Entity")
+          assert(response.body.isLeft)
+          assert(response.body.left.get ===
+            "Could not process the uploaded deployment pack file. Reason: requirement failed: " +
+            "A ResolvedRuntimeConfig needs exactly one bundle with startLevel '0', but this one has (distinct): 0")
+        }
       }
-    }
 
-    s"Multipart POST with minimal profile (one bundles) should succeed" in logException {
-      withServer { server =>
-        assert(packFile.exists() === true)
+      s"Multipart POST with minimal profile (one bundles) should succeed" in logException {
+        withServer { server =>
+          assert(packFile.exists() === true)
 
-        val response = sttp.sttp.
-          multipartBody(sttp.multipartFile("file", packFile)).
-          auth.basic("tester", "mysecret").
-          post(uploadUrl).
-          send()
+          val response = sttp.sttp.
+            multipartBody(sttp.multipartFile("file", packFile)).
+            auth.basic("tester", "mysecret").
+            post(uploadUrl).
+            send()
 
-        log.info("body: " + response.body)
-        log.info("headers: " + response.headers)
-        log.info("response: " + response)
+          log.info("body: " + response.body)
+          log.info("headers: " + response.headers)
+          log.info("response: " + response)
 
-        assert(response.code === 200)
-        assert(response.statusText === "OK")
-        assert(response.body.isRight)
-        assert(response.body.right.get === "\"Uploaded profile test.pack.minimal 1.0.0\"")
+          assert(response.code === 200)
+          assert(response.statusText === "OK")
+          assert(response.body.isRight)
+          assert(response.body.right.get === "\"Uploaded profile test.pack.minimal 1.0.0\"")
 
-        // We expect the bundle file in the local repo
-        assert(new File(server.dir, "repositories/artifacts/org/example/fake/1.0.0/fake-1.0.0.jar").exists())
+          // We expect the bundle file in the local repo
+          assert(new File(server.dir, "repositories/artifacts/org/example/fake/1.0.0/fake-1.0.0.jar").exists())
 
-        // We expect the profile in the profile repo
-        assert(new File(server.dir, "repositories/rcs/test.pack.minimal-1.0.0.conf").exists())
+          // We expect the profile in the profile repo
+          assert(new File(server.dir, "repositories/rcs/test.pack.minimal-1.0.0.conf").exists())
+        }
       }
     }
   }
