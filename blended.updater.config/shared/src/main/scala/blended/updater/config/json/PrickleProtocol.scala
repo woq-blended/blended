7e777d6881520d833d9fc92323ec4e582bc3b548
Tobias Roeser <le.petit.fou@web.de>
2018-12-03 12:08:20 +0100

    Sorted imports

--- blended.updater.config/shared/src/main/scala/blended/updater/config/json/PrickleProtocol.scala
+++ blended.updater.config/shared/src/main/scala/blended/updater/config/json/PrickleProtocol.scala
@@ -1,22 +1,25 @@
 package blended.updater.config.json
 
-import blended.updater.config.ServiceInfo
-import blended.updater.config.ContainerInfo
-import blended.updater.config.OverlayState
-import blended.updater.config.UpdateAction
 import blended.updater.config.ActivateProfile
-import blended.updater.config.StageProfile
-import blended.updater.config.AddRuntimeConfig
 import blended.updater.config.AddOverlayConfig
+import blended.updater.config.AddRuntimeConfig
+import blended.updater.config.ContainerInfo
+import blended.updater.config.OverlayConfig
+import blended.updater.config.OverlayState
+import blended.updater.config.RuntimeConfig
+import blended.updater.config.ServiceInfo
+import blended.updater.config.StageProfile
+import blended.updater.config.UpdateAction
 import prickle._
 import microjson.JsValue
-import blended.updater.config.RuntimeConfig
-import blended.updater.config.OverlayConfig
 
+/**
+ * Import this object to have Prickle JSON pickers and unpicklers in implicit scope.
+ */
 object PrickleProtocol {
-  
+
   implicit val prickleConfig: PConfig[JsValue] = JsConfig(areSharedObjectsSupported = false)
-  
+
   // sealed trait needs special handling
   implicit val updateActionPickler: PicklerPair[UpdateAction] = CompositePickler[UpdateAction].
     concreteType[ActivateProfile].
@@ -31,17 +34,17 @@ object PrickleProtocol {
 
   implicit val containerInfoPickler: Pickler[ContainerInfo] = Pickler.materializePickler[ContainerInfo]
   implicit val containerInfoUnpickler: Unpickler[ContainerInfo] = Unpickler.materializeUnpickler[ContainerInfo]
-  
+
   implicit val overlayStatePickler: PicklerPair[OverlayState] = CompositePickler[OverlayState].
     concreteType[OverlayState.Active.type].
     concreteType[OverlayState.Valid.type].
     concreteType[OverlayState.Invalid.type].
     concreteType[OverlayState.Pending.type]
-  
+
   implicit val runtimeConfigPickler: Pickler[RuntimeConfig] = Pickler.materializePickler[RuntimeConfig]
   implicit val runtimeConfigUnpickler: Unpickler[RuntimeConfig] = Unpickler.materializeUnpickler[RuntimeConfig]
-  
+
   implicit val overlayConfigPickler: Pickler[OverlayConfig] = Pickler.materializePickler[OverlayConfig]
   implicit val overlayConfigUnpickler: Unpickler[OverlayConfig] = Unpickler.materializeUnpickler[OverlayConfig]
-  
+
 }
