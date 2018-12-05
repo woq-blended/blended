e9d6e00d309eec0974714f15d5c736df64fd0d07
Tobias Roeser <le.petit.fou@web.de>
2018-12-05 09:13:19 +0100

    Fixed plugin publish location

--- scripts/build-prerequisites.sh
+++ scripts/build-prerequisites.sh
@@ -2,5 +2,5 @@
 
 git clone --branch master --depth 1 "https://github.com/woq-blended/sbt-testlogconfig.git" "${TRAVIS_BUILD_DIR}/sbt-testlogconfig"
 cd "${TRAVIS_BUILD_DIR}/sbt-testlogconfig"
-sbt publishM2
+sbt publishLocal
 cd "${TRAVIS_BUILD_DIR}"
