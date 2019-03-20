e806daeaede753fef07fd280aacb7693b2867cb4
Tobias Roeser <le.petit.fou@web.de>
2019-03-20 14:38:25 +0100

    Also run publishLocal

--- scripts/buildITest.sh
+++ scripts/buildITest.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/woq-blended/blended.itestsupport.git $TRAVIS_BUILD_DIR/blendedITest
 cd $TRAVIS_BUILD_DIR/blendedITest
 git checkout master
-sbt update publishM2
+sbt update publishLocal publishM2
 cd $TRAVIS_BUILD_DIR
--- scripts/buildUI.sh
+++ scripts/buildUI.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/woq-blended/blended.mgmt.ui.git $TRAVIS_BUILD_DIR/mgmtUi
 cd $TRAVIS_BUILD_DIR/mgmtUi
 git checkout master
-sbt update test publishM2
+sbt update test publishLocal publishM2
 cd $TRAVIS_BUILD_DIR
