9201ab0c052f923e8eced94d3baa496e4df905a5
Andreas Gies <andreas@wayofquality.de>
2019-04-11 15:16:57 +0200

    Bumping Gogo Shell version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,10 +64,10 @@ trait Dependencies {
   val domino = "com.github.domino-osgi" %% "domino" % dominoVersion
 
   val felixConnect = "org.apache.felix" % "org.apache.felix.connect" % "0.1.0"
-  val felixGogoCommand = "org.apache.felix" % "org.apache.felix.gogo.command" % "1.0.2"
-  val felixGogoJline = "org.apache.felix" % "org.apache.felix.gogo.jline" % "1.1.0"
-  val felixGogoShell = "org.apache.felix" % "org.apache.felix.gogo.shell" % "1.1.0"
-  val felixGogoRuntime = "org.apache.felix" % "org.apache.felix.gogo.runtime" % "1.1.0"
+  val felixGogoCommand = "org.apache.felix" % "org.apache.felix.gogo.command" % "1.1.0"
+  val felixGogoJline = "org.apache.felix" % "org.apache.felix.gogo.jline" % "1.1.4"
+  val felixGogoShell = "org.apache.felix" % "org.apache.felix.gogo.shell" % "1.1.2"
+  val felixGogoRuntime = "org.apache.felix" % "org.apache.felix.gogo.runtime" % "1.1.2"
   val felixFileinstall = "org.apache.felix" % "org.apache.felix.fileinstall" % "3.4.2"
   val felixFramework = "org.apache.felix" % "org.apache.felix.framework" % "5.6.10"
 
