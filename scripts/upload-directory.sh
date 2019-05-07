0e7e5a423b9bf11380cdb547e5691239a44a13ed
Tobias Roeser <le.petit.fou@web.de>
2019-05-07 07:04:50 +0200

    Fixed upload script

--- scripts/upload-directory.sh
+++ scripts/upload-directory.sh
@@ -5,9 +5,10 @@ set -x
 UPLOAD_DIR=$1
 UPLOAD_FILE=$2
 
+LAST_DIR=$(pwd)
 cd $UPLOAD_DIR
 tar -czf ../$UPLOAD_FILE.tgz .
 
 $TRAVIS_BUILD_DIR/scripts/dropbox_uploader.sh upload ../$UPLOAD_FILE.tgz travis/$TRAVIS_BUILD_NUMBER/$UPLOAD_FILE.tgz
 
-$TRAVIS_BUILD_DIR
+cd $LAST_DIR
