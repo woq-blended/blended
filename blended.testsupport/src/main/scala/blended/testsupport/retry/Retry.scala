ab960646a580f8e31d767fd67d02be6ee5e45d72
Tobias Roeser <le.petit.fou@web.de>
2019-09-24 16:43:40 +0200

    Fixed typos

--- blended.testsupport/src/main/scala/blended/testsupport/retry/Retry.scala
+++ blended.testsupport/src/main/scala/blended/testsupport/retry/Retry.scala
@@ -15,7 +15,7 @@ object Retry {
    * @param retries The max count of retries, before giving up.
    * @param onRetry Action to be run before a retry.
    * @param op The operation to be executed and, iff failed, retried.
-   * @param ex ExecutionContext to run the inner futures with.
+   * @param ec ExecutionContext to run the inner futures with.
    * @param s The Scheduler used to schedule the next retry.
    *
    * @return The Future containing the result of `op` or the last failure.
