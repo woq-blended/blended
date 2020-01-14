fbd94dd5776aef5fbb8fc6fc8af26b72725067d6
Andreas Gies <andreas@wayofquality.de>
2020-01-14 15:23:48 +0100

    fixing library path for scep client

--- project/BlendedSecurityScepStandalone.scala
+++ project/BlendedSecurityScepStandalone.scala
@@ -9,7 +9,7 @@ import de.wayofquality.sbt.testlogconfig.TestLogConfig.autoImport._
 
 object BlendedSecurityScepStandalone extends ProjectFactory {
 
-  private[this] val libDir = "libs"
+  private[this] val libDir = "lib"
 
   implicit class ImplicitModuleId(moduleId: ModuleID) {
     def pure: ModuleID = moduleId.withExclusions(Vector(InclExclRule()))
